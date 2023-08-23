aeb36cf5795450f136ccb7bfb225b5127601fcf1
k143632 <peder.kongelf@nav.no>
2023-08-23 13:33:27 +0200

    initial

---
+++ src/main/java/no/nav/ebxmlprocessor/EbxmlProcessorApplication.java
@@ -0,0 +1,13 @@
+package no.nav.ebxmlprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbxmlProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbxmlProcessorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/nav/ebxmlprocessor/EbxmlProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package no.nav.ebxmlprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbxmlProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
