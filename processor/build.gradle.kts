f9cd5e1092bd6c560a3ea21eeff14c88bb308736
Alexander Petrov <alexander.petrov@nav.no>
2023-09-01 14:06:11 +0200

    Hello world ktor

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -3,8 +3,9 @@
  */
 
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm")
     application
+    id("io.ktor.plugin") version "2.3.4"
 }
 
 repositories {
@@ -16,6 +17,8 @@ tasks.register<Wrapper>("wrapper") {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-server-core:2.3.4")
+    implementation("io.ktor:ktor-server-netty:2.3.4")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
--- processor/src/main/kotlin/ebxml/processor/app/App.kt
+++ processor/src/main/kotlin/ebxml/processor/app/App.kt
@@ -3,6 +3,18 @@
  */
 package ebxml.processor.app
 
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
 fun main() {
-    println("Hello World")
+    embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!")
+            }
+        }
+    }.start(wait = true)
 }
