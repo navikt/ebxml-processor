062c1c7834ab437af63966607871d4b1e95187dd
Alexander Petrov <alexander.petrov@nav.no>
2023-09-05 13:37:04 +0200

    legg til emotakk-protokoll

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core:2.3.4")
     implementation("io.ktor:ktor-server-netty:2.3.4")
     implementation("com.github.labai:labai-jsr305x-annotations:0.0.2")
+	implementation("no.nav.emottak:ebxml-protokoll:0.0.4")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
