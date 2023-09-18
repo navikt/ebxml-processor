3ee878386e022d215a4a219879ff1e9bce48fd75
Alexander Petrov <alexander.petrov@nav.no>
2023-09-18 13:03:51 +0200

    legg til driver

--- cpa-repo/build.gradle.kts
+++ cpa-repo/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-java-time:0.43.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+
+    runtimeOnly("org.postgresql:postgresql:42.6.0")
 }
 
 application {
