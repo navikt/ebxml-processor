0f0890531aa1b17de9779a9ec3a548241d252f22
Alexander Petrov <alexander.petrov@nav.no>
2023-09-21 15:39:14 +0200

    sette opp database i ebms-provider appen

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,15 +23,25 @@ tasks.test {
 }
 
 dependencies {
+    implementation(project(":felles"))
     implementation("io.ktor:ktor-server-core:2.3.4")
     implementation("io.ktor:ktor-server-netty:2.3.4")
     implementation("com.github.labai:labai-jsr305x-annotations:0.0.2")
     implementation("jakarta.xml.bind:jakarta.xml.bind-api:4.0.0", )
     implementation("org.glassfish.jaxb:jaxb-runtime:2.4.0-b180830.0438")
+    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("org.flywaydb:flyway-core:9.16.3")
+    implementation("org.jetbrains.exposed:exposed-core:0.43.0")
+    implementation("org.jetbrains.exposed:exposed-dao:0.43.0")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.43.0")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.43.0")
     //implementation("org.glassfish.jaxb:jaxb-runtime:4.0.3") // TODO: Latest. Krever at protokoll oppdateres
     implementation("no.nav.emottak:ebxml-protokoll:0.0.4")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+
+    runtimeOnly("org.postgresql:postgresql:42.6.0")
+
 }
 
 application {
