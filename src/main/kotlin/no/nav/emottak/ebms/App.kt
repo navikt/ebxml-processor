1aecf82decffc8b3f49ffbdbab31bf44124fc8db
Alexander Petrov <alexander.petrov@nav.no>
2023-09-22 15:07:07 +0200

    EbmsAcknowledgment + ebmsmessage tabel
    Co-authored-by: Alexander Petrov <alexander.petrov@nav.no>
    Co-authored-by: Chris Olsen <christopher.olsen@nav.no>

--- src/main/kotlin/no/nav/emottak/ebms/App.kt
+++ src/main/kotlin/no/nav/emottak/ebms/App.kt
@@ -22,54 +22,70 @@ import no.nav.emottak.ebms.processing.EbmsMessageProcessor
 import no.nav.emottak.ebms.xml.xmlMarshaller
 import org.xmlsoap.schemas.soap.envelope.Envelope
 
+
+val processor = EbmsMessageProcessor()
 fun main() {
 
-    val processor = EbmsMessageProcessor()
+
     val database = Database(mapHikariConfig(DatabaseConfig()))
     database.migrate()
 
-    embeddedServer(Netty, port = 8080) {
-        routing {
-            get("/") {
-                call.respondText("Hello, world!")
-            }
-            post("/ebms") {
-                val allParts = call.receiveMultipart().readAllParts()
-                val dokument = allParts.find {
-                    it.contentType?.toString() == "text/xml" && it.contentDisposition == null
-                }
-                val attachments = allParts.filter { it.contentDisposition == ContentDisposition.Attachment}
-                val dokumentWithAttachment = EbMSDocument("",(dokument as PartData.FormItem).payload(),attachments.map { EbMSAttachment( (it as PartData.FormItem).payload(), it.contentType!!.contentType ,"contentId") })
-                processor.process(dokumentWithAttachment)
-                println(dokumentWithAttachment)
-
-                call.respondText("Hello")
-            }
-            post("/ebmsTest") {
-                val allParts = call.receiveMultipart().readAllParts()
-                val dokument = allParts.find {
-                    it.contentType?.toString() == "text/xml" && it.contentDisposition == null
-                }
-                val envelope = xmlMarshaller.unmarshal(String((dokument as PartData.FormItem).payload()), Envelope::class.java)
-
-                val conversationId = envelope.getConversationId()
-                println(conversationId)
-                val attachmentId = envelope.getAttachmentId()
-                println(attachmentId)
-                val attachments = allParts
-                    .filter { it.contentDisposition == ContentDisposition.Attachment }
-                    .filter { it.headers.get("Content-Id")?.contains(attachmentId, true) ?: false }
-                    .map { (it as PartData.FormItem).payload() }
-                    .first()
-                println(
-                    String(attachments)
-                )
-                call.respondText("Hello2")
-            }
-        }
-    }.start(wait = true)
+    embeddedServer(Netty, port = 8080, module = Application::myApplicationModule).start(wait = true)
 }
 
 fun PartData.FormItem.payload() : ByteArray {
     return java.util.Base64.getMimeDecoder().decode(this.value)
+}
+
+fun Application.myApplicationModule() {
+    routing {
+        get("/") {
+            call.respondText("Hello, world!")
+        }
+        post("/ebms") {
+            val allParts = call.receiveMultipart().readAllParts()
+            val dokument = allParts.find {
+                it.contentType?.toString() == "text/xml" && it.contentDisposition == null
+            }
+            val attachments = allParts.filter { it.contentDisposition == ContentDisposition.Attachment }
+            val dokumentWithAttachment = EbMSDocument(
+                "",
+                (dokument as PartData.FormItem).payload(),
+                attachments.map {
+                    EbMSAttachment(
+                        (it as PartData.FormItem).payload(),
+                        it.contentType!!.contentType,
+                        "contentId"
+                    )
+                })
+            processor.process(dokumentWithAttachment)
+            println(dokumentWithAttachment)
+
+            call.respondText("Hello")
+        }
+
+
+        post("/ebmsTest") {
+            val allParts = call.receiveMultipart().readAllParts()
+            val dokument = allParts.find {
+                it.contentType?.toString() == "text/xml" && it.contentDisposition == null
+            }
+            val envelope =
+                xmlMarshaller.unmarshal(String((dokument as PartData.FormItem).payload()), Envelope::class.java)
+
+            val conversationId = envelope.getConversationId()
+            println(conversationId)
+            val attachmentId = envelope.getAttachmentId()
+            println(attachmentId)
+            val attachments = allParts
+                .filter { it.contentDisposition == ContentDisposition.Attachment }
+                .filter { it.headers.get("Content-Id")?.contains(attachmentId, true) ?: false }
+                .map { (it as PartData.FormItem).payload() }
+                .first()
+            println(
+                String(attachments)
+            )
+            call.respondText("Hello2")
+        }
+    }
 }
