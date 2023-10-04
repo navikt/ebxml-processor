e947336312cbdd7767c3ccac6ede211c55b41936
Thomas Burnett <thburnett@users.noreply.github.com>
2023-10-04 15:38:26 +0200

    Bytter egendefinerte constants med xml sec constants

--- felles/src/main/kotlin/no/nav/emottak/util/signatur/Constants.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.emottak.util.signatur
-
-const val XMLDSIG_ENVELOPED_SIGNATURE = "http://www.w3.org/2000/09/xmldsig#enveloped-signature"
-const val REC_XPATH = "http://www.w3.org/TR/1999/REC-xpath-19991116"
-const val REC_XML = "http://www.w3.org/TR/2001/REC-xml-c14n-20010315"
-const val SIGNATURE_ALGORITHM_SHA256= "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256"
-const val SIGNATURE_ALGORITHM_SHA1= "http://www.w3.org/2000/09/xmldsig#rsa-sha1"
-const val DIGEST_ALGORITHM_SHA256= "http://www.w3.org/2001/04/xmlenc#sha256"
-const val DIGEST_ALGORITHM_SHA1= "http://www.w3.org/2000/09/xmlenc#sha1"
