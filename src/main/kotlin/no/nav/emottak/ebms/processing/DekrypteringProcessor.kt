f76bd8a71ad87a36c935bc96ef9395050d9592df
Thomas Burnett <thburnett@users.noreply.github.com>
2023-10-12 09:22:31 +0200

    Fikser refaktorert import og feilende tester

--- src/main/kotlin/no/nav/emottak/ebms/processing/DekrypteringProcessor.kt
+++ src/main/kotlin/no/nav/emottak/ebms/processing/DekrypteringProcessor.kt
@@ -1,14 +1,14 @@
 package no.nav.emottak.ebms.processing
 
 import no.nav.emottak.ebms.model.EbMSError
-import no.nav.emottak.ebms.model.EbMSMessage
+import no.nav.emottak.ebms.model.EbMSPayloadMessage
 import no.nav.emottak.util.crypto.DecryptionException
 import no.nav.emottak.util.crypto.Dekryptering
 import no.nav.emottak.util.marker
 
 
 private val dekryptering = Dekryptering()
-class DekrypteringProcessor(ebMSMessage: EbMSMessage): Processor(ebMSMessage) {
+class DekrypteringProcessor(override val ebMSMessage: EbMSPayloadMessage): Processor(ebMSMessage) {
     override fun process() {
         //TODO om payload ikke er kryptert
         var payloadReference = "Ukjent"
