9b91026f0215acd52b8a2de9328d693f61e907c3
Alexander Petrov <alexander.petrov@nav.no>
2023-11-06 16:45:55 +0100

    Vi trenger å akkumulere feil fra mikrotjenestene til ebxml-provideren

--- src/main/kotlin/no/nav/emottak/melding/model/Payload.kt
+++ src/main/kotlin/no/nav/emottak/melding/model/Payload.kt
@@ -1,6 +1,9 @@
 package no.nav.emottak.melding.model
 
 import kotlinx.serialization.Serializable
+import org.oasis_open.committees.ebxml_msg.schema.msg_header_2_0.Description
+import org.oasis_open.committees.ebxml_msg.schema.msg_header_2_0.ErrorList
+import org.oasis_open.committees.ebxml_msg.schema.msg_header_2_0.SeverityType
 
 
 @Serializable
@@ -16,7 +19,29 @@ data class PayloadResponse(
 )
 
 @Serializable
-data class Error(val message:String)
+data class Error(val code:ErrorCode,
+                 val descriptionText:String,
+                 val sevirity:String) {
+
+    fun asEbxmlError(location: String? = null):org.oasis_open.committees.ebxml_msg.schema.msg_header_2_0.Error {
+            val error = org.oasis_open.committees.ebxml_msg.schema.msg_header_2_0.Error()
+            error.errorCode = this.code.value
+            val description = Description()
+            description.lang = "no" // Default verdi fra spec.
+            description.value = descriptionText
+            error.description = description
+
+            error.severity = this.sevirity.takeIf { sevirity!=null}?.let {
+                SeverityType.fromValue(it) } ?: SeverityType.ERROR
+            error.location = location // Content-ID hvis error er i Payload. Hvis ebxml så er det XPath
+            error.id = "ERROR_ID" // Element Id
+            //error.any             // Unused?
+            //error.otherAttributes // Unused?
+            //error.codeContext = "urn:oasis:names:tc:ebxml-msg:service:errors" // Skal være default ifølge spec. Trenger ikke overstyre / sette
+            return error
+        }
+
+}
 
 
 @Serializable
@@ -38,6 +63,64 @@ data class Party(
 )
 
 @Serializable
-data class ValidationResult(
+data class Processing(val retry:Int = 0) {
+
+}
+
+@Serializable
+data class ValidationResponse(
     val valid: Boolean
+    /*,
+    val signingCertificate: SignatureDetails,
+    val encryptionCertificate: ByteArray,
+    val processing: Processing,
+    val error: Error?
+*/
 )
+
+
+ enum class ErrorCode(val value:String,val description:String) {
+        VALUE_NOT_RECOGNIZED("ValueNotRecognized","Element content or attribute value not recognized."),
+        NOT_SUPPORTED("NotSupported","Element content or attribute not supported"),
+        INCONSISTENT("ValueNotRecognized","Element content or attribute value inconsistent with other elements or attributes."),
+        OTHER_XML("OtherXml","Other error in an element content or attribute value"),
+        DELIVERY_FAILURE("DeliveryFailure","Message Delivery Failure"),
+        TIME_TO_LIVE_EXPIRED("TimeToLiveExpired","Message Time To Live Expired"),
+        SECURITY_FAILURE("SecurityFailure","Message Security Checks Failed"),
+        MIME_PROBLEM("MimeProblem","URI resolve error"),
+        UNKNOWN("Unknown","Unknown Error");
+
+     fun createEbxmlError(descriptionText:String, severityType: SeverityType? = null, location: String? = null):org.oasis_open.committees.ebxml_msg.schema.msg_header_2_0.Error {
+            val error = org.oasis_open.committees.ebxml_msg.schema.msg_header_2_0.Error()
+            error.errorCode = this.value
+            val description = Description()
+            description.lang = "no" // Default verdi fra spec.
+            description.value = descriptionText
+            error.description = description
+
+            error.severity = severityType ?: SeverityType.ERROR
+            error.location = location // Content-ID hvis error er i Payload. Hvis ebxml så er det XPath
+            error.id = "ERROR_ID" // Element Id
+            //error.any             // Unused?
+            //error.otherAttributes // Unused?
+            //error.codeContext = "urn:oasis:names:tc:ebxml-msg:service:errors" // Skal være default ifølge spec. Trenger ikke overstyre / sette
+            return error
+        }
+
+
+ }
+
+fun List<org.oasis_open.committees.ebxml_msg.schema.msg_header_2_0.Error>.asErrorList(): ErrorList {
+            if(this.isEmpty()) {
+                throw IllegalArgumentException("(4.2.3 Kan ikke opprette ErrorList uten errors")
+            }
+            val errorList = ErrorList()
+            errorList.error.addAll(this)
+            errorList.version = "2.0"
+            //errorList.any // Unused?
+            errorList.id // "May be used for error tracking"
+            errorList.highestSeverity = this.sortedBy {
+                it.severity == SeverityType.ERROR }.first().severity
+            errorList.isMustUnderstand = true; // Alltid
+            return errorList
+}
