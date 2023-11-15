0d52b8523f40cfa7272b6aa3a19fcf2c637197ad
Thomas Burnett <thburnett@users.noreply.github.com>
2023-11-15 11:59:02 +0100

    Henter message header ved hjelp av namespace

--- felles/src/main/kotlin/no/nav/emottak/Constants.kt
+++ felles/src/main/kotlin/no/nav/emottak/Constants.kt
@@ -3,3 +3,6 @@ package no.nav.emottak
 const val EBMS_SERVICE_URI = "urn:oasis:names:tc:ebxml-msg:service"
 const val ACKNOWLEDGMENT_ACTION = "Acknowledgment"
 const val MESSAGE_ERROR_ACTION = "MessageError"
+
+const val OASIS_EBXML_MSG_HEADER_XSD_NS_URI = "http://www.oasis-open.org/committees/ebxml-msg/schema/msg-header-2_0.xsd"
+const val OASIS_EBXML_MSG_HEADER_TAG = "MessageHeader"
