5809e6d35a72fc731fa630fef2560ed30d0bb322
Thomas Burnett <thburnett@users.noreply.github.com>
2023-11-17 13:00:09 +0100

    Rydder i constants og fikser gradle dependencies

---
+++ src/main/kotlin/no/nav/emottak/constants/MimeHeaders.kt
@@ -0,0 +1,10 @@
+package no.nav.emottak.constants
+
+object MimeHeaders {
+    const val MIME_VERSION                  = "MIME-Version"
+    const val SOAP_ACTION                   = "SOAPAction"
+    const val CONTENT_TYPE                  = "Content-Type"
+    const val CONTENT_ID                    = "Content-Id"
+    const val CONTENT_TRANSFER_ENCODING     = "Content-Transfer-Encoding"
+    const val CONTENT_DISPOSITION           = "Content-Disposition"
+}
