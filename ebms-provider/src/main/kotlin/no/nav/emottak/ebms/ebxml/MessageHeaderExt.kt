53230f9fd7bf7a096a5e38a57d3ce2fefcdd9853
Chris Olsen <christopher.olsen@nav.no>
2023-12-11 15:26:06 +0100

    Fjernet placeholder from/to role.

--- ebms-provider/src/main/kotlin/no/nav/emottak/ebms/ebxml/MessageHeaderExt.kt
+++ ebms-provider/src/main/kotlin/no/nav/emottak/ebms/ebxml/MessageHeaderExt.kt
@@ -11,16 +11,16 @@ import org.oasis_open.committees.ebxml_msg.schema.msg_header_2_0.To
 import java.time.Instant
 import java.util.*
 
-fun MessageHeader.createResponseHeader(newFromRole: String, newToRole: String, newAction: String?, newService: String?): MessageHeader {
+fun MessageHeader.createResponseHeader(newAction: String?, newService: String?): MessageHeader {
     val messageHeader = MessageHeader()
     messageHeader.conversationId = this.conversationId
     messageHeader.from = From().also {
         it.partyId.addAll(this.to.partyId)
-        it.role = newFromRole
+        it.role = this.to.role
     }
     messageHeader.to = To().also {
         it.partyId.addAll(this.from.partyId)
-        it.role = newToRole
+        it.role = this.from.role
     }
     messageHeader.service = if (newService != null) Service().also { it.value = newService } else this.service
     messageHeader.action = newAction ?: this.action
