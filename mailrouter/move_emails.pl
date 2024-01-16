37be00b480d6c61ec91b5fc59ac9d1432bb1b912
Thomas Burnett <thburnett@users.noreply.github.com>
2024-01-16 15:54:07 +0100

    Enkel feilhåndtering og litt penere output

--- mailrouter/move_emails.pl
+++ mailrouter/move_emails.pl
@@ -7,17 +7,19 @@ use List::Util qw(any);
 use MIME::Parser;
 use autodie;
 
-my ($inputDirectory, $newEmottakDirectory, $oldEmottakDirectory) = @ARGV;
+my ($inputDirectory, $newEmottakDirectory, $oldEmottakDirectory, $errorDirectory) = @ARGV;
 
 if (
     not defined $inputDirectory or
     not defined $newEmottakDirectory or
-    not defined $oldEmottakDirectory
+    not defined $oldEmottakDirectory or
+    not defined $errorDirectory
 ) {
-    die "Need directory parameters (call with command line arguments 'inputDirectory' 'newEmottakDirectory' 'oldEmottakDirectory'\n";
+    die "Need directory parameters (call with command line arguments 'inputDirectory' 'newEmottakDirectory' 'oldEmottakDirectory' 'errorDirectory\n";
 }
 
 my $dryRunMode = 1;
+
 if ($dryRunMode ne 0) {
     print "OBS! dryRunMode active, will not actually move any files!\n";
 }
@@ -28,14 +30,15 @@ my %messageTypes = (
     'testType' => ["testService", "testAction"]
 );
 
-printf "Input directory       :  %s\n", $inputDirectory;
-printf "New eMottak directory :  %s\n", $newEmottakDirectory;
-printf "Old eMottak directory :  %s\n", $oldEmottakDirectory;
+printf "Input directory             : %s\n", $inputDirectory;
+printf "New eMottak directory       : %s\n", $newEmottakDirectory;
+printf "Old eMottak directory       : %s\n\n", $oldEmottakDirectory;
 
 opendir(DIR, $inputDirectory) or die "Can't open $inputDirectory: $!";
 
 my $newCounter = 0;
 my $oldCounter = 0;
+my $errorCounter = 0;
 my $fileCounter = 0;
 
 foreach my $filename (readdir(DIR)) {
@@ -44,15 +47,25 @@ foreach my $filename (readdir(DIR)) {
         my $parser = MIME::Parser->new;
         $parser->output_to_core(1); #ikke skriv fil til disk
 
-        # Leser epost
-        my $entity = $parser->parse_open("$inputDirectory/$filename");
-        # $entity->dump_skeleton();
-        my $first_part = $entity->parts(0);
-        my $body = (defined $first_part) ? $first_part->bodyhandle->as_string : $entity->bodyhandle->as_string;
+        my $dom = eval {
+            # Leser epost
+            my $entity = $parser->parse_open("$inputDirectory/$filename");
+            # $entity->dump_skeleton();
+            my $first_part = $entity->parts(0);
+            my $body = (defined $first_part) ? $first_part->bodyhandle->as_string : $entity->bodyhandle->as_string;
 
-        # Leser ebxml dokument
-        my $xml_parser = XML::LibXML->new;
-        my $dom = $xml_parser->parse_string($body);
+            # Leser ebxml dokument
+            my $xml_parser = XML::LibXML->new;
+            $xml_parser->parse_string($body);
+        };
+        if ($@ ne '') {
+            if ($dryRunMode eq 0) {
+                move("$inputDirectory/$filename", "$errorDirectory/$filename");
+            }
+            printf "Message '%s' failed with error %s", $filename, $@;
+            $errorCounter++;
+            next;
+        };
 
         my $xpc = XML::LibXML::XPathContext->new($dom);
         $xpc->registerNs('soap',  'http://schemas.xmlsoap.org/soap/envelope/');
@@ -87,7 +100,9 @@ foreach my $filename (readdir(DIR)) {
     }
 }
 
-printf "%s of %s files moved to new system\n", $newCounter, $fileCounter;
-printf "%s of %s files moved to old system\n", $oldCounter, $fileCounter;
+printf "\nMessages processed          : %s/%s\n", $newCounter+$oldCounter+$errorCounter, $fileCounter;
+printf "Messages sent to new system : %s\n", $newCounter;
+printf "Messages sent to old system : %s\n", $oldCounter;
+printf "Messages sent to error      : %s\n", $errorCounter;
 
 closedir(DIR);
