32f19b6e8a144ab0eba9c279803cdd15ba18f01d
Alexander Petrov <alexander.petrov@nav.no>
2024-02-08 15:30:44 +0100

    Kontrakt endring på Payload
    Co-authored-by: Thomas Burnett <thomas.hoberg.burnett@nav.no>

--- src/main/kotlin/no/nav/emottak/util/LoggingUtil.kt
+++ src/main/kotlin/no/nav/emottak/util/LoggingUtil.kt
@@ -14,7 +14,9 @@ import no.nav.emottak.constants.LogIndex.TO_PARTY
 import no.nav.emottak.constants.LogIndex.TO_ROLE
 import no.nav.emottak.constants.LogIndex.X_MAILER
 import no.nav.emottak.constants.SMTPHeaders
+import no.nav.emottak.melding.model.Addressing
 import no.nav.emottak.melding.model.Header
+import no.nav.emottak.melding.model.PayloadRequest
 import no.nav.emottak.melding.model.SignatureDetailsRequest
 import no.nav.emottak.melding.model.ValidationRequest
 import org.oasis_open.committees.ebxml_msg.schema.msg_header_2_0.MessageHeader
@@ -30,6 +32,13 @@ fun Header.marker(): LogstashMarker = Markers.appendEntries(
         Pair(FROM_ROLE, this.from.role),
         Pair(TO_PARTY, "${this.to.partyId.firstOrNull()?.type ?: UKJENT_VERDI}:${this.to.partyId.firstOrNull()?.value ?: UKJENT_VERDI}"),
         Pair(FROM_PARTY, "${this.from.partyId.firstOrNull()?.type ?: UKJENT_VERDI}:${this.from.partyId.firstOrNull()?.value ?: UKJENT_VERDI}"),
+    )
+)
+
+fun PayloadRequest.marker(): LogstashMarker = Markers.appendEntries(
+    mapOf(
+        Pair(MARKER_MOTTAK_ID, this.messageId),
+        Pair(MARKER_CONVERSATION_ID, this.conversationId),
     )
 )
 
