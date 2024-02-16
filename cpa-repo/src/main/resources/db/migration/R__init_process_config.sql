239f613a61510871c285109f1cb740666f81bac0
Chris Olsen <christopher.olsen@nav.no>
2024-02-16 16:44:53 +0100

    fix table

--- cpa-repo/src/main/resources/db/migration/R__init_process_config.sql
+++ cpa-repo/src/main/resources/db/migration/R__init_process_config.sql
@@ -10,12 +10,11 @@ values ('Behandler',
         TRUE,
            /* ADAPTER refererer til ekstra custom processering, nesten ingen flyter bruker det. */
         'LoggEgenandel')
-on conflict ("ROLE", "SERVICE", "ACTION") do update set
-                          "KRYPTERING"   = EXCLUDED."KRYPTERING",
-                          "KOMPRIMERING" = EXCLUDED."KOMPRIMERING",
-                          "SIGNERING"    = EXCLUDED."SIGNERING",
-                          "INTERNFORMAT" = EXCLUDED."INTERNFORMAT",
-                          "VALIDERING"   = EXCLUDED."VALIDERING",
-                          "APPREC"       = EXCLUDED."APPREC",
+on conflict (ROLE, SERVICE, ACTION) do update set KRYPTERING   = EXCLUDED.KRYPTERING,
+                                                  KOMPRIMERING = EXCLUDED.KOMPRIMERING,
+                                                  SIGNERING    = EXCLUDED.SIGNERING,
+                                                  INTERNFORMAT = EXCLUDED.INTERNFORMAT,
+                                                  VALIDERING   = EXCLUDED.VALIDERING,
+                                                  APPREC       = EXCLUDED.APPREC,
     /* ADAPTER refererer til ekstra custom processering, nesten ingen bruker det. */
-                          "ADAPTER"      = EXCLUDED."ADAPTER";
+                                                  ADAPTER      = EXCLUDED.ADAPTER;
