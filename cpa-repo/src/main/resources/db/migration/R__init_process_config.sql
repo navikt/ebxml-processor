7af3a9be6bb432ba5404772804f1259b57032b55
Alexander Petrov <alexander.petrov@nav.no>
2024-02-22 14:13:03 +0100

    leg til Avisning og Svar til process execution

--- cpa-repo/src/main/resources/db/migration/R__init_process_config.sql
+++ cpa-repo/src/main/resources/db/migration/R__init_process_config.sql
@@ -10,6 +10,42 @@ values ('Behandler',
         TRUE,
            /* ADAPTER refererer til ekstra custom processering, nesten ingen flyter bruker det. */
         'LoggEgenandel')
+insert into process_config
+values ('Utlever',
+        'HarBorgerEgenandelFritak',
+        'EgenandelForesporsel',
+        false,
+        false,
+        false,
+        TRUE,
+        TRUE,
+        TRUE,
+           /* ADAPTER refererer til ekstra custom processering, nesten ingen flyter bruker det. */
+        'LoggEgenandel')
+insert into process_config
+values ('Frikortregister',
+        'HarBorgerEgenandelFritak',
+        'Svar',
+        false,
+        false,
+        false,
+        TRUE,
+        false,
+        false,
+           /* ADAPTER refererer til ekstra custom processering, nesten ingen flyter bruker det. */
+        null)
+insert into process_config
+values ('Frikortregister',
+        'HarBorgerEgenandelFritak',
+        'Avvisning',
+        false,
+        false,
+        false,
+        TRUE,
+        false,
+        false,
+           /* ADAPTER refererer til ekstra custom processering, nesten ingen flyter bruker det. */
+        null)
 on conflict (ROLE, SERVICE, ACTION) do update set KRYPTERING   = EXCLUDED.KRYPTERING,
                                                   KOMPRIMERING = EXCLUDED.KOMPRIMERING,
                                                   SIGNERING    = EXCLUDED.SIGNERING,
