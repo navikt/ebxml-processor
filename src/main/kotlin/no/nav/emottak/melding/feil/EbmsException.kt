93271957d422a3e0c82e96b4969e28089c882004
Alexander Petrov <alexander.petrov@nav.no>
2024-02-28 14:44:51 +0100

    refaktorering

--- src/main/kotlin/no/nav/emottak/melding/feil/EbmsException.kt
+++ src/main/kotlin/no/nav/emottak/melding/feil/EbmsException.kt
@@ -5,10 +5,25 @@ import no.nav.emottak.melding.model.Feil
 import org.oasis_open.committees.ebxml_msg.schema.msg_header_2_0.SeverityType
 
 
-open class EbmsException(val feil: List<Feil>, exception: Throwable? = null) : Exception(exception) {
-    constructor(descriptionText:String,
-                         errorCode: ErrorCode = ErrorCode.UNKNOWN,
-                         severity: String = SeverityType.ERROR.value(),
-                         exception: Throwable? = null) : this(listOf(Feil(errorCode,descriptionText,severity)),exception)
+open class EbmsException(
+    val feil: List<Feil>,
+    exception: Throwable? = null
+) : Exception( concatFeilmessage(feil),exception) {
+
+     constructor(
+        message: String,
+        errorCode: ErrorCode = ErrorCode.UNKNOWN,
+        severity: String = SeverityType.ERROR.value(),
+        exception: Throwable? = null
+    ) : this( listOf(Feil(errorCode, message, severity)), exception)
+
+    companion object {
+        fun concatFeilmessage(feil:List<Feil>) =
+                feil.joinToString(separator = ",") {
+                    it.descriptionText
+                }
+    }
+
+
 
 }
