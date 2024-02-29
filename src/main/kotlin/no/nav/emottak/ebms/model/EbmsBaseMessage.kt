6dfd287bb1e984177c9eac001440ac7866a950ff
Alexander Petrov <alexander.petrov@nav.no>
2024-02-29 14:31:08 +0100

    slett klasser vi ikke bruker lenge
    (cherry picked from commit 563046dd49c8206e009ec2cd5cb0a4411fe5b9b6)

--- src/main/kotlin/no/nav/emottak/ebms/model/EbmsBaseMessage.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.emottak.ebms.model
-
-import org.oasis_open.committees.ebxml_msg.schema.msg_header_2_0.MessageHeader
-import org.w3c.dom.Document
-
-interface EbmsBaseMessage {
-    val messageHeader: MessageHeader
-    val dokument: Document?
-}
