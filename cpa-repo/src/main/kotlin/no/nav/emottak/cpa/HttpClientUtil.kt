651d98489542321f87c0b26abcac69ba5f5e91a3
Alexander Petrov <alexander.petrov@nav.no>
2024-03-04 15:33:04 +0100

    lint

--- cpa-repo/src/main/kotlin/no/nav/emottak/cpa/HttpClientUtil.kt
+++ cpa-repo/src/main/kotlin/no/nav/emottak/cpa/HttpClientUtil.kt
@@ -2,11 +2,11 @@ package no.nav.emottak.cpa
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.*
+import io.ktor.client.plugins.HttpRequestRetry
+import no.nav.emottak.util.getEnvVar
 import java.net.InetSocketAddress
 import java.net.Proxy
 import java.net.URL
-import no.nav.emottak.util.getEnvVar
 
 val httpProxyUrl = getEnvVar("HTTPS_PROXY", "")
 
@@ -17,7 +17,7 @@ class HttpClientUtil {
             install(HttpRequestRetry) {
                 retryOnServerErrors(maxRetries = 5)
                 exponentialDelay()
-            } 
+            }
             engine {
                 if (httpProxyUrl.isNotBlank()) {
                     proxy = Proxy(Proxy.Type.HTTP, InetSocketAddress(URL(httpProxyUrl).host, URL(httpProxyUrl).port))
