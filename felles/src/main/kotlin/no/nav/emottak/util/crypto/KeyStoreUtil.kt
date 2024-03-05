29155bd7ef6dda6c35243de20b202d5685ed911a
Alexander Petrov <alexander.petrov@nav.no>
2024-03-05 14:07:22 +0100

    fjern

--- felles/src/main/kotlin/no/nav/emottak/util/crypto/KeyStoreUtil.kt
+++ felles/src/main/kotlin/no/nav/emottak/util/crypto/KeyStoreUtil.kt
@@ -70,7 +70,6 @@ internal class KeyStoreUtil {
                 log.error("Unable to load keystore $storePath falling back to truststore",e)
                 ByteArrayInputStream(this::class.java.classLoader.getResource("truststore.p12").readBytes())
             }
-        log.info("loggppp " + String(storePass))
         keyStore!!.load(fileContent, storePass)
         return keyStore
     }
