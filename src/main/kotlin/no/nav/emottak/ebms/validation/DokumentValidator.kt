a84373531371fc36196525b423c6763c052d84d7
Alexander Petrov <alexander.petrov@nav.no>
2024-03-26 13:26:01 +0100

    namespace agnostic xpath

--- src/main/kotlin/no/nav/emottak/ebms/validation/DokumentValidator.kt
+++ src/main/kotlin/no/nav/emottak/ebms/validation/DokumentValidator.kt
@@ -1,50 +1,57 @@
 package no.nav.emottak.ebms.validation
 
-import jakarta.xml.bind.JAXBElement
-import javax.xml.transform.dom.DOMResult
-import javax.xml.xpath.XPath
-import javax.xml.xpath.XPathFactory
 import kotlinx.coroutines.runBlocking
 import no.kith.xmlstds.msghead._2006_05_24.MsgHead
 import no.nav.emottak.ebms.CpaRepoClient
 import no.nav.emottak.ebms.model.EbmsMessage
 import no.nav.emottak.ebms.model.PayloadMessage
-import no.nav.emottak.ebms.xml.XmlMarshaller
 import no.nav.emottak.ebms.xml.unmarshal
 import no.nav.emottak.melding.feil.EbmsException
 import no.nav.emottak.melding.model.ErrorCode
 import no.nav.emottak.melding.model.Feil
 import no.nav.emottak.melding.model.ValidationRequest
 import no.nav.emottak.melding.model.ValidationResult
-import no.nav.emottak.util.decodeBase64
 import org.slf4j.LoggerFactory
-import org.w3c.dom.Document
+import org.w3c.dom.Element
+import javax.xml.xpath.XPath
+import javax.xml.xpath.XPathFactory
 
 val log = LoggerFactory.getLogger("no.nav.emottak.ebms.DokumentValidator")
+
 class DokumentValidator(val httpClient: CpaRepoClient) {
 
     fun validateIn(message: EbmsMessage) = validate(message, true)
     fun validateOut(message: EbmsMessage) = validate(message, false)
 
-
     private fun shouldThrowExceptionForTestPurposes(bytes: ByteArray) {
         val fnr = try {
+            log.info("Evaluering av kandidat på feil signal test")
             val payloadMsgHead = unmarshal(String(bytes), MsgHead::class.java)
-            val egenandelforesporsel = payloadMsgHead.document.first().refDoc.content.any.first() as JAXBElement<*>
-            val res = DOMResult()
-            XmlMarshaller().marshal(egenandelforesporsel, res)
-            val document: Document = res.node as Document
+            val egenandelforesporsel = payloadMsgHead.document.first().refDoc.content.any.first() as Element
             val xPath: XPath = XPathFactory.newInstance().newXPath()
-            val borgerFnrExpression = xPath.compile("/EgenandelForesporselV2/HarBorgerEgenandelfritak/BorgerFnr")
-            borgerFnrExpression.evaluate(document)
+            val borgerFnrExpressionV1 =
+                xPath.compile("""/*[local-name() = 'EgenandelForesporsel']/*[local-name() = 'HarBorgerFrikort']/*[local-name() = 'BorgerFnr']/text()""")
+            val borgerFnrExpressionV2 =
+                xPath.compile("""/*[local-name() = 'EgenandelForesporselV2']/*[local-name() = 'HarBorgerFrikort']/*[local-name() = 'BorgerFnr']/text()""")
+
+            log.info("Evaluating for version1: ${borgerFnrExpressionV1.evaluate(egenandelforesporsel.ownerDocument)}")
+            log.info("Evaluating for version2: ${borgerFnrExpressionV2.evaluate(egenandelforesporsel.ownerDocument)}")
+
+            borgerFnrExpressionV1.evaluate(egenandelforesporsel.ownerDocument).takeIf { !it.isNullOrBlank() }
+                ?: borgerFnrExpressionV2.evaluate(egenandelforesporsel.ownerDocument)
         } catch (e: Exception) {
+            log.error("Klarer ikke å parse dokumenten via xpath", e)
             ""
         }
         if (fnr == "20118690681") throw RuntimeException("Dette er et test fnr 20118690681, kaster exception")
     }
 
     private fun validate(message: EbmsMessage, sjekSignature: Boolean): ValidationResult {
-        val validationRequest = ValidationRequest(message.messageId, message.conversationId, message.cpaId, message.addressing)
+        if (message is PayloadMessage) {
+            shouldThrowExceptionForTestPurposes(message.payload.bytes)
+        }
+        val validationRequest =
+            ValidationRequest(message.messageId, message.conversationId, message.cpaId, message.addressing)
         val validationResult = runBlocking {
             httpClient.postValidate(message.requestId, validationRequest)
         }
@@ -52,9 +59,9 @@ class DokumentValidator(val httpClient: CpaRepoClient) {
         if (!validationResult.valid()) throw EbmsException(validationResult.error!!)
         if (sjekSignature) {
             runCatching {
-                shouldThrowExceptionForTestPurposes((message as PayloadMessage).payload.bytes)
                 message.sjekkSignature(validationResult.payloadProcessing!!.signingCertificate)
             }.onFailure {
+                log.error("Signatursjekk har feilet", it)
                 throw EbmsException(
                     (validationResult.error ?: listOf()) + listOf(
                         Feil(
