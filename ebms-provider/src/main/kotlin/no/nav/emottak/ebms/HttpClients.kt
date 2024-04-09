21a9139e34b5dc1a457c7b1c9771d33356720012
Ivan Skodje <ivanskodje@protonmail.com>
2024-04-09 11:16:57 +0200

    fix ktlint problemer

--- ebms-provider/src/main/kotlin/no/nav/emottak/ebms/HttpClients.kt
+++ ebms-provider/src/main/kotlin/no/nav/emottak/ebms/HttpClients.kt
@@ -1,19 +1,29 @@
 package no.nav.emottak.ebms
 
 import com.nimbusds.jwt.SignedJWT
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.auth.*
-import io.ktor.client.plugins.auth.providers.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.http.headers
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import no.nav.emottak.auth.AZURE_AD_AUTH
 import no.nav.emottak.auth.AuthConfig
-import no.nav.emottak.melding.model.*
+import no.nav.emottak.melding.model.PayloadRequest
+import no.nav.emottak.melding.model.PayloadResponse
+import no.nav.emottak.melding.model.SendInRequest
+import no.nav.emottak.melding.model.SendInResponse
+import no.nav.emottak.melding.model.ValidationRequest
+import no.nav.emottak.melding.model.ValidationResult
 import no.nav.emottak.util.getEnvVar
 
 class CpaRepoClient(clientProvider: () -> HttpClient) {
