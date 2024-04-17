eb1480f29f0141d1f856457350665f376e751877
Alexander Petrov <alexander.petrov@nav.no>
2024-04-17 13:48:30 +0200

    klint

--- src/main/kotlin/no/nav/emottak/fellesformat/FellesFormatWrapper.kt
+++ src/main/kotlin/no/nav/emottak/fellesformat/FellesFormatWrapper.kt
@@ -1,18 +1,18 @@
 package no.nav.emottak.fellesformat
 
 import no.kith.xmlstds.msghead._2006_05_24.MsgHead
+import no.nav.emottak.ebms.log
 import no.nav.emottak.frikort.unmarshal
+import no.nav.emottak.frikort.xmlMarshaller
 import no.nav.emottak.melding.model.Addressing
 import no.nav.emottak.melding.model.Party
 import no.nav.emottak.melding.model.PartyId
 import no.nav.emottak.melding.model.SendInRequest
+import no.nav.emottak.util.getEnvVar
 import no.nav.emottak.util.toXMLGregorianCalendar
 import no.trygdeetaten.xml.eiff._1.EIFellesformat
 import no.trygdeetaten.xml.eiff._1.ObjectFactory
 import java.time.Instant
-import no.nav.emottak.ebms.log
-import no.nav.emottak.frikort.xmlMarshaller
-import no.nav.emottak.util.getEnvVar
 
 private val fellesFormatFactory = ObjectFactory()
 
@@ -31,7 +31,7 @@ fun wrapMessageInEIFellesFormat(sendInRequest: SendInRequest): EIFellesformat =
         it.mottakenhetBlokk = createFellesFormatMottakEnhetBlokk(sendInRequest.messageId, sendInRequest.conversationId, sendInRequest.addressing)
         it.msgHead = unmarshal(sendInRequest.payload.toString(Charsets.UTF_8), MsgHead::class.java)
     }.also {
-        if (getEnvVar("NAIS_CLUSTER_NAME") != "prod-fss") {
+        if (getEnvVar("NAIS_CLUSTER_NAME","local") != "prod-fss") {
             log.info("Sending in request to frikort with body " + xmlMarshaller.marshal(it))
         }
     }
--- src/main/kotlin/no/nav/emottak/frikort/FrikortClient.kt
+++ src/main/kotlin/no/nav/emottak/frikort/FrikortClient.kt
@@ -1,13 +1,13 @@
 package no.nav.emottak.frikort
 
 import no.nav.emottak.cxf.ServiceBuilder
+import no.nav.emottak.ebms.log
+import no.nav.emottak.util.getEnvVar
 import no.nav.tjeneste.ekstern.frikort.v1.FrikortV1Port
 import no.nav.tjeneste.ekstern.frikort.v1.types.FrikortsporringResponse
 import no.nav.tjeneste.ekstern.frikort.v1.types.ObjectFactory
 import no.trygdeetaten.xml.eiff._1.EIFellesformat
 import javax.xml.namespace.QName
-import no.nav.emottak.ebms.log
-import no.nav.emottak.util.getEnvVar
 
 val frikortClient = frikortEndpoint()
 private val frikortObjectFactory = ObjectFactory()
@@ -25,7 +25,7 @@ fun frikortEndpoint(): FrikortV1Port =
 fun frikortsporring(fellesformat: EIFellesformat): FrikortsporringResponse = frikortClient.frikortsporring(
     frikortObjectFactory.createFrikortsporringRequest().also { it.eiFellesformat = fellesformat }
 ).also {
-    if (getEnvVar("NAIS_CLUSTER_NAME") != "prod-fss") {
-        log.info("Send in Frikort response " + xmlMarshaller.marshal(it) )
+    if (getEnvVar("NAIS_CLUSTER_NAME","local") != "prod-fss") {
+        log.info("Send in Frikort response " + xmlMarshaller.marshal(it))
     }
 }
