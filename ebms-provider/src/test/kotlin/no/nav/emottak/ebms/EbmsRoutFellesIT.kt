b11a45e875ce174609b732e794653a3802821722
Chris Olsen <christopher.olsen@nav.no>
2024-04-25 13:51:48 +0200

    Full hostname på service discovery. Kanskje conflict med webproxy?

--- ebms-provider/src/test/kotlin/no/nav/emottak/ebms/EbmsRoutFellesIT.kt
+++ ebms-provider/src/test/kotlin/no/nav/emottak/ebms/EbmsRoutFellesIT.kt
@@ -73,7 +73,7 @@ abstract class EbmsRoutFellesIT(val endpoint: String) {
             }
         }
         externalServices {
-            hosts(getEnvVar("CPA_REPO_URL", "http://cpa-repo")) {
+            hosts(getEnvVar("CPA_REPO_URL", "http://cpa-repo.team-emottak.svc.nais.local")) {
                 this.install(ContentNegotiation) {
                     json()
                 }
