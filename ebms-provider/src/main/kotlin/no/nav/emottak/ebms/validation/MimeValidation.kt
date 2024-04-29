363e5d07bf9198b4514a81a4bf87a760f55421b5
Alexander Petrov <alexander.petrov@nav.no>
2024-04-29 15:09:47 +0200

    endre til Content-ID

--- ebms-provider/src/main/kotlin/no/nav/emottak/ebms/validation/MimeValidation.kt
+++ ebms-provider/src/main/kotlin/no/nav/emottak/ebms/validation/MimeValidation.kt
@@ -16,7 +16,7 @@ object MimeHeaders {
     const val MIME_VERSION = "MIME-Version"
     const val SOAP_ACTION = "SOAPAction"
     const val CONTENT_TYPE = "Content-Type"
-    const val CONTENT_ID = "Content-Id"
+    const val CONTENT_ID = "Content-ID"
     const val CONTENT_TRANSFER_ENCODING = "Content-Transfer-Encoding"
     const val CONTENT_DISPOSITION = "Content-Disposition"
 }
