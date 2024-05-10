f45da88ee976ff93e7aa0eca87be101ef33aee1a
Thomas Burnett <thburnett@users.noreply.github.com>
2024-05-10 10:01:32 +0200

    Loggjustering av meldingsbehandling

--- src/main/kotlin/no/nav/emottak/ebms/util/LoggingUtil.kt
+++ src/main/kotlin/no/nav/emottak/ebms/util/LoggingUtil.kt
@@ -4,19 +4,17 @@ import net.logstash.logback.marker.LogstashMarker
 import net.logstash.logback.marker.Markers
 import no.nav.emottak.constants.LogIndex
 import no.nav.emottak.ebms.model.EbmsMessage
-import no.nav.emottak.util.UKJENT_VERDI
 
-fun EbmsMessage.marker(loggableHeaderPairs: List<Pair<String, String>> = emptyList()): LogstashMarker = Markers.appendEntries(
+fun EbmsMessage.marker(loggableHeaderPairs: Map<String, String> = emptyMap()): LogstashMarker = Markers.appendEntries(
     mapOf(
-        Pair(LogIndex.MARKER_MOTTAK_ID, this.messageId),
-        Pair(LogIndex.MARKER_CONVERSATION_ID, this.conversationId),
-        Pair(LogIndex.CPA_ID, this.cpaId ?: UKJENT_VERDI),
-        Pair(LogIndex.SERVICE, this.addressing.service ?: UKJENT_VERDI),
-        Pair(LogIndex.ACTION, this.addressing.action),
-        Pair(LogIndex.TO_ROLE, this.addressing.to.role ?: UKJENT_VERDI),
-        Pair(LogIndex.FROM_ROLE, this.addressing.from.role ?: UKJENT_VERDI),
-        Pair(LogIndex.TO_PARTY, (this.addressing.to.partyId.firstOrNull()?.type + ":" + this.addressing.to.partyId.firstOrNull()?.value)),
-        Pair(LogIndex.FROM_PARTY, (this.addressing.from.partyId.firstOrNull()?.type + ":" + this.addressing.from.partyId.firstOrNull()?.value)),
-        *loggableHeaderPairs.toTypedArray()
-    )
+        LogIndex.MARKER_MESSAGE_ID to this.messageId,
+        LogIndex.MARKER_CONVERSATION_ID to this.conversationId,
+        LogIndex.CPA_ID to this.cpaId,
+        LogIndex.SERVICE to this.addressing.service,
+        LogIndex.ACTION to this.addressing.action,
+        LogIndex.TO_ROLE to this.addressing.to.role,
+        LogIndex.FROM_ROLE to this.addressing.from.role,
+        LogIndex.TO_PARTY to (this.addressing.to.partyId.firstOrNull()?.type + ":" + this.addressing.to.partyId.firstOrNull()?.value),
+        LogIndex.FROM_PARTY to (this.addressing.from.partyId.firstOrNull()?.type + ":" + this.addressing.from.partyId.firstOrNull()?.value)
+    ) + loggableHeaderPairs
 )
