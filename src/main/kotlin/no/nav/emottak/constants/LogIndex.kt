a8b699212a0d79c65faaff0301c5b19cc22e78d3
Alexander Petrov <alexander.petrov@nav.no>
2024-05-10 13:09:31 +0200

    refaktor fields

--- src/main/kotlin/no/nav/emottak/constants/LogIndex.kt
+++ src/main/kotlin/no/nav/emottak/constants/LogIndex.kt
@@ -1,14 +1,14 @@
 package no.nav.emottak.constants
 
 object LogIndex {
-    const val MARKER_MESSAGE_ID = "messageId"
-    const val MARKER_CONVERSATION_ID = "ebConversationId"
-    const val FROM_PARTY = "ebAvsenderId"
-    const val TO_PARTY = "ebMottakerId"
-    const val TO_ROLE = "toRole"
+    const val MESSAGE_ID = "messageId"
+    const val CONVERSATION_ID = "conversationId"
+    const val FROM_PARTY = "avsenderId"
+    const val TO_PARTY = "mottakerId"
+    const val TO_ROLE = "role"
     const val FROM_ROLE = "fromRole"
-    const val SERVICE = "ebxmlService"
+    const val SERVICE = "service"
     const val CPA_ID = "cpaId"
-    const val ACTION = "ebxmlAction"
+    const val ACTION = "action"
     const val X_MAILER = "systemkilde"
 }
