a4b653fcc11678ea6512716c3aab499e5efbdc35
Thomas Burnett <thburnett@users.noreply.github.com>
2024-05-13 15:58:03 +0200

    Token for payload client

--- ebms-provider/src/main/kotlin/no/nav/emottak/ebms/HttpClients.kt
+++ ebms-provider/src/main/kotlin/no/nav/emottak/ebms/HttpClients.kt
@@ -1,11 +1,22 @@
 package no.nav.emottak.ebms
 
+import com.nimbusds.jwt.SignedJWT
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.header
+import io.ktor.client.request.headers
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import no.nav.emottak.melding.model.PayloadRequest
 import no.nav.emottak.melding.model.PayloadResponse
 import no.nav.emottak.melding.model.SendInRequest
@@ -13,6 +24,9 @@ import no.nav.emottak.melding.model.SendInResponse
 import no.nav.emottak.melding.model.ValidationRequest
 import no.nav.emottak.melding.model.ValidationResult
 import no.nav.emottak.util.getEnvVar
+import java.net.InetSocketAddress
+import java.net.Proxy
+import java.net.URI
 
 class CpaRepoClient(clientProvider: () -> HttpClient) {
     private var httpClient = clientProvider.invoke()
@@ -48,5 +62,89 @@ class SendInClient(clientProvider: () -> HttpClient) {
             setBody(sendInRequest)
             contentType(ContentType.Application.Json)
         }.body()
+    }
+}
+
+val EBMS_SEND_IN_SCOPE = getEnvVar(
+    "EBMS_SEND_IN_SCOPE",
+    "api://" + getEnvVar("NAIS_CLUSTER_NAME", "dev-fss") + ".team-emottak.ebms-send-in/.default"
+)
+val EBMS_PAYLOAD_SCOPE = getEnvVar(
+    "EBMS_PAYLOAD_SCOPE",
+    "api://" + getEnvVar("NAIS_CLUSTER_NAME", "dev-fss") + ".team-emottak.ebms-payload/.default"
+)
+
+fun defaultHttpClient(): () -> HttpClient {
+    return {
+        HttpClient(CIO) {
+            expectSuccess = true
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+    }
+}
+
+const val AZURE_AD_AUTH = "AZURE_AD"
+val LENIENT_JSON_PARSER = Json {
+    isLenient = true
+}
+
+fun scopedAuthHttpClient(
+    scope: String
+): () -> HttpClient {
+    return {
+        HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json()
+            }
+            install(Auth) {
+                bearer {
+                    refreshTokens {
+                        proxiedHttpClient().post(
+                            getEnvVar(
+                                "AZURE_OPENID_CONFIG_TOKEN_ENDPOINT",
+                                "http://localhost:3344/$AZURE_AD_AUTH/token"
+                            )
+                        ) {
+                            headers {
+                                header("Content-Type", "application/x-www-form-urlencoded")
+                            }
+                            setBody(
+                                "client_id=" + getEnvVar("AZURE_APP_CLIENT_ID", "dummyclient") +
+                                    "&client_secret=" + getEnvVar("AZURE_APP_CLIENT_SECRET", "dummysecret") +
+                                    "&scope=" + scope +
+                                    "&grant_type=client_credentials"
+                            )
+                        }.bodyAsText()
+                            .let { tokenResponseString ->
+                                // log.info("The token response string we received was: $tokenResponseString")
+                                SignedJWT.parse(
+                                    LENIENT_JSON_PARSER.decodeFromString<Map<String, String>>(tokenResponseString)["access_token"] as String
+                                )
+                            }
+                            .let { parsedJwt ->
+                                // log.info("After parsing it, we got: $parsedJwt")
+                                BearerTokens(parsedJwt.serialize(), "refresh token is unused")
+                            }
+                    }
+                    sendWithoutRequest {
+                        true
+                    }
+                }
+            }
+        }
+    }
+}
+
+private fun proxiedHttpClient() = HttpClient(CIO) {
+    engine {
+        val httpProxyUrl = getEnvVar("HTTP_PROXY", "")
+        if (httpProxyUrl.isNotBlank()) {
+            proxy = Proxy(
+                Proxy.Type.HTTP,
+                InetSocketAddress(URI(httpProxyUrl).toURL().host, URI(httpProxyUrl).toURL().port)
+            )
+        }
     }
 }
