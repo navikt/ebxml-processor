36e8e98cdc6c8a79cad2ead8da00e0b947318bc5
Alexander Petrov <alexander.petrov@nav.no>
2024-05-15 15:25:57 +0200

    Revert "log ebxml + payload headers"
    This reverts commit 4dccdf40a7298bffa0d3c765a22f64e3b519d6e4.

--- src/main/kotlin/no/nav/emottak/smtp/App.kt
+++ src/main/kotlin/no/nav/emottak/smtp/App.kt
@@ -7,6 +7,7 @@ import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 import org.slf4j.LoggerFactory
 
