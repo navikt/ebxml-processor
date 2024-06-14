c1532b566e3b0500e3b9c9a5e8045f6d7c352a5a
Alexander Petrov <alexander.petrov@nav.no>
2024-06-14 13:14:37 +0200

    with debug lines

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,8 +19,8 @@ dependencyResolutionManagement {
             version("jsch", "0.2.16")
             version("cxf", "3.5.5")
 
-            library("bcpkix-jdk18on", "org.bouncycastle", "bcpkix-jdk18on").versionRef("bouncycastle")
-            library("bcprov-jdk18on", "org.bouncycastle", "bcprov-jdk18on").versionRef("bouncycastle")
+            library("bcpkix-jdk18on", "org.bouncycastle", "bcpkix-debug-jdk18on").versionRef("bouncycastle")
+            library("bcprov-jdk18on", "org.bouncycastle", "bcprov-debug-jdk18on").versionRef("bouncycastle")
             library("apache-santuario", "org.apache.santuario:xmlsec:3.0.2")
 
             library("exposed-core", "org.jetbrains.exposed", "exposed-core").versionRef("exposed")
