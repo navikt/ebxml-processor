b42e04a3f6e47faa6dfb549ea433d5737eeea502
Chris Olsen <christopher.olsen@nav.no>
2024-09-10 10:13:25 +0200

    Feilhåndtering

--- src/main/kotlin/no/nav/emottak/utbetaling/UtbetalingClient.kt
+++ src/main/kotlin/no/nav/emottak/utbetaling/UtbetalingClient.kt
@@ -3,6 +3,11 @@ package no.nav.emottak.utbetaling
 import no.kith.xmlstds.msghead._2006_05_24.MsgHead
 import no.nav.ekstern.virkemiddelokonomi.tjenester.utbetaling.v1.FinnBrukersUtbetalteYtelser
 import no.nav.ekstern.virkemiddelokonomi.tjenester.utbetaling.v1.FinnUtbetalingListe
+import no.nav.ekstern.virkemiddelokonomi.tjenester.utbetaling.v1.FinnUtbetalingListeBaksystemIkkeTilgjengelig
+import no.nav.ekstern.virkemiddelokonomi.tjenester.utbetaling.v1.FinnUtbetalingListeBrukerIkkeFunnet
+import no.nav.ekstern.virkemiddelokonomi.tjenester.utbetaling.v1.FinnUtbetalingListeIngenTilgangTilEnEllerFlereYtelser
+import no.nav.ekstern.virkemiddelokonomi.tjenester.utbetaling.v1.FinnUtbetalingListeUgyldigDato
+import no.nav.ekstern.virkemiddelokonomi.tjenester.utbetaling.v1.FinnUtbetalingListeUgyldigKombinasjonBrukerIdOgBrukertype
 import no.nav.emottak.cxf.ServiceBuilder
 import no.nav.emottak.util.getEnvVar
 import org.slf4j.LoggerFactory
@@ -12,6 +17,9 @@ import javax.xml.namespace.QName
 class InntektsForesporselClient {
 
     val log = LoggerFactory.getLogger(InntektsForesporselClient::class.java)
+
+    val utbetalingObjectFactory: no.nav.ekstern.virkemiddelokonomi.tjenester.utbetaling.v1.ObjectFactory =
+        no.nav.ekstern.virkemiddelokonomi.tjenester.utbetaling.v1.ObjectFactory()
 
     val YRP_URL_TEST = "https://ytelser-rest-proxy.intern.dev.nav.no"
     val YRP_URL_PROD = "https://ytelser-rest-proxy.intern.nav.no"
@@ -26,14 +34,40 @@ class InntektsForesporselClient {
 
     fun behandleInntektsforesporsel(payloadBytes: ByteArray): ByteArray {
         val msgHead = utbetalingXmlMarshaller.unmarshal(String(payloadBytes), MsgHead::class.java)
-        val melding = msgHead.document.map { it.refDoc.content.any }.also { if (it.size > 1) log.warn("Inntektsforesporsel refdoc har size >1") }
+        val melding = msgHead.document.map { it.refDoc.content.any }
+            .also { if (it.size > 1) log.warn("Inntektsforesporsel refdoc har size >1") }
             .first().also { if (it.size > 1) log.warn("Inntektsforesporsel content har size >1") }.first()
-        val response: Any = when (melding) {
-            is FinnUtbetalingListe -> inntektsforesporselSoapEndpoint.finnUtbetalingListe(melding.request)
-            is FinnBrukersUtbetalteYtelser -> inntektsforesporselSoapEndpoint.finnBrukersUtbetalteYtelser(melding.request)
-            else -> throw IllegalStateException("Ukjent meldingstype. Classname: " + melding.javaClass.name)
+        try {
+            val response: Any = when (melding) {
+                is FinnUtbetalingListe -> inntektsforesporselSoapEndpoint.finnUtbetalingListe(melding.request)
+                is FinnBrukersUtbetalteYtelser -> inntektsforesporselSoapEndpoint.finnBrukersUtbetalteYtelser(melding.request)
+                else -> throw IllegalStateException("Ukjent meldingstype. Classname: " + melding.javaClass.name)
+            }
+            return marshal(response).toByteArray()
+        } catch (utbetalError: Throwable) {
+            log.info("Handling inntektsforesporsel error: " + utbetalError.message)
+            return utbetalingXmlMarshaller.marshalToByteArray(
+                when (utbetalError) {
+                    is FinnUtbetalingListeBrukerIkkeFunnet
+                    -> utbetalingObjectFactory
+                        .createFinnUtbetalingListebrukerIkkeFunnet(utbetalError.faultInfo)
+                    is FinnUtbetalingListeBaksystemIkkeTilgjengelig
+                    -> utbetalingObjectFactory
+                        .createFinnUtbetalingListebaksystemIkkeTilgjengelig(utbetalError.faultInfo)
+                    is FinnUtbetalingListeIngenTilgangTilEnEllerFlereYtelser
+                    -> utbetalingObjectFactory
+                        .createFinnUtbetalingListeingenTilgangTilEnEllerFlereYtelser(utbetalError.faultInfo)
+                    is FinnUtbetalingListeUgyldigDato
+                    -> utbetalingObjectFactory
+                        .createFinnUtbetalingListeugyldigDato(utbetalError.faultInfo)
+                    is FinnUtbetalingListeUgyldigKombinasjonBrukerIdOgBrukertype
+                    -> utbetalingObjectFactory
+                        .createFinnUtbetalingListeugyldigKombinasjonBrukerIdOgBrukertype(utbetalError.faultInfo)
+                    else ->
+                        throw utbetalError.also { log.error("Ukjent feiltype: " + it.message, it) }
+                }
+            )
         }
-        return marshal(response).toByteArray()
     }
 }
 
