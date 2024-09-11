130bf94e5aabae963103417725b050cc265ec092
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2024-09-11 17:14:11 +0200

    ktlint

--- src/main/kotlin/no/nav/emottak/smtp/cpasync/CpaSyncService.kt
+++ src/main/kotlin/no/nav/emottak/smtp/cpasync/CpaSyncService.kt
@@ -61,8 +61,9 @@ class CpaSyncService(private val cpaRepoClient: HttpClient, private val nfsConne
         val cpaId = getCpaIdFromCpaContent(cpaContent)
 
         if (cpaId == null) {
-            log.warn("Regex to find CPA ID in file ${nfsCpaFile.filename} did not find any match. " +
-                        "File corrupted or wrongful regex. Aborting sync."
+            log.warn(
+                "Regex to find CPA ID in file ${nfsCpaFile.filename} did not find any match. " +
+                    "File corrupted or wrongful regex. Aborting sync."
             )
             return null
         }
