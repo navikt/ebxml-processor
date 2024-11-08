00a7b79e548e6953e7a58b68d0594ee3110f2bd7
OleksandrChmyrNAV <oleksandr.chmyr@nav.no>
2024-11-08 10:13:32 +0100

    Lagt til en enkel juridisk logg klient (#50)
    * Lagt til en enkel juridisk logg klient
    
    * Lagt til coroutine context for forespørsel til juridisk logg.
    
    * Forandret JuridiskLoggRequest bygging logikken
    
    * Fikset PayloadIntegrationTest
    
    * Lagt til extra logging
    
    * Revert "Lagt til extra logging"
    
    This reverts commit 52a11ea7bae160146263dd66e451abfc090dd22e.

---
+++ ebms-payload/init/init.sh
@@ -0,0 +1,2 @@
+export JURIDESKLOGG_USERNAME=$(cat /var/run/secrets/nais.io/vault/serviceuser/username)
+export JURIDESKLOGG_PASSWORD=$(cat /var/run/secrets/nais.io/vault/serviceuser/password)
---
+++ ebms-payload/src/main/kotlin/no/nav/emottak/payload/juridisklogg/JuridiskLoggService.kt
@@ -0,0 +1,73 @@
+package no.nav.emottak.payload.juridisklogg
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.request.basicAuth
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.Serializable
+import no.nav.emottak.message.model.Direction
+import no.nav.emottak.message.model.PayloadRequest
+import no.nav.emottak.payload.log
+import no.nav.emottak.util.getEnvVar
+
+class JuridiskLoggService() {
+    private val juridiskLoggUrl = getEnvVar("APP_JURIDISKLOGG_URI", "https://app-q1.adeo.no/juridisklogg") + "/api/rest/logg"
+    private val juridiskLoggStorageTime = getEnvVar("JURIDISKLOGG_STORAGE_TIME_YEARS", "1").toInt()
+    private val userName = getEnvVar("JURIDESKLOGG_USERNAME", "dummyUsername")
+    private val userPassword = getEnvVar("JURIDESKLOGG_PASSWORD", "dummyPassword")
+
+    init {
+        log.debug("Juridisk logg URL: $juridiskLoggUrl")
+        log.debug("Juridisk logg user: $userName")
+        log.debug("Juridisk logg password length: ${userPassword.length}")
+    }
+
+    fun logge(payloadRequest: PayloadRequest) {
+        val httpClient = HttpClient(CIO)
+        val request = JuridiskLoggRequest(
+            payloadRequest.messageId,
+            if (payloadRequest.direction == Direction.IN) "Ekstern bruker" else "NAV",
+            if (payloadRequest.direction == Direction.IN) "NAV" else "Ekstern bruker",
+            juridiskLoggStorageTime,
+            payloadRequest.payload.bytes
+        )
+        log.debug("Juridisk logg forespørsel: $request")
+
+        val response = suspend {
+            withContext(Dispatchers.IO) {
+                try {
+                    httpClient.post(juridiskLoggUrl) {
+                        setBody(request)
+                        contentType(ContentType.Application.Json)
+                        basicAuth(userName, userPassword)
+                    }.body<JuridiskLoggResponse>()
+                } catch (e: Exception) {
+                    log.error("Feil med å sende forespørsel til juridisk logg", e)
+                } finally {
+                    httpClient.close()
+                }
+            }
+        }
+        log.debug("Juridisk logg respons: $response")
+    }
+}
+
+@Serializable
+data class JuridiskLoggRequest(
+    val meldingsId: String,
+    val avsender: String,
+    val mottaker: String,
+    val antallAarLagres: Int = 10,
+    val meldingsInnhold: ByteArray
+)
+
+@Serializable
+data class JuridiskLoggResponse(
+    val id: String
+)
