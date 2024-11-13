73d20e6a6813faa155799027fcaff8c418148fe4
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2024-11-13 12:43:11 +0100

    Fikset Base 64 kryptering

--- ebms-payload/src/main/kotlin/no/nav/emottak/payload/juridisklogg/JuridiskLoggService.kt
+++ ebms-payload/src/main/kotlin/no/nav/emottak/payload/juridisklogg/JuridiskLoggService.kt
@@ -43,7 +43,7 @@ class JuridiskLoggService() {
             if (payloadRequest.direction == Direction.IN) "Ekstern bruker" else "NAV",
             if (payloadRequest.direction == Direction.IN) "NAV" else "Ekstern bruker",
             juridiskLoggStorageTime,
-            java.util.Base64.getMimeEncoder().encodeToString(payloadRequest.payload.bytes)
+            java.util.Base64.getEncoder().encodeToString(payloadRequest.payload.bytes)
         )
         log.debug("Juridisk logg forespørsel: $request")
 
