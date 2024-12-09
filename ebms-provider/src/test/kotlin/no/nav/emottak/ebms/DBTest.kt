8b9897e8861a4d3ca634a898c683deda30338b4e
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2024-12-09 13:54:51 +0100

    Lagt til database configurasjon for test

--- ebms-provider/src/test/kotlin/no/nav/emottak/ebms/DBTest.kt
+++ ebms-provider/src/test/kotlin/no/nav/emottak/ebms/DBTest.kt
@@ -2,11 +2,23 @@ package no.nav.emottak.ebms
 
 import com.zaxxer.hikari.HikariConfig
 import no.nav.emottak.cpa.persistence.CPA_DB_NAME
+import no.nav.emottak.ebms.persistence.EBMS_DB_NAME
 import org.testcontainers.containers.PostgreSQLContainer
 
 fun cpaPostgres(): PostgreSQLContainer<Nothing> =
     PostgreSQLContainer<Nothing>("postgres:14").apply {
         withUsername("$CPA_DB_NAME-admin")
+        withReuse(true)
+        withLabel("app-navn", "cpa-repo")
+        start()
+        println(
+            "Databasen er startet opp, portnummer: $firstMappedPort, jdbcUrl: jdbc:postgresql://localhost:$firstMappedPort/test, credentials: test og test"
+        )
+    }
+
+fun ebmsPostgres(): PostgreSQLContainer<Nothing> =
+    PostgreSQLContainer<Nothing>("postgres:14").apply {
+        withUsername("$EBMS_DB_NAME-admin")
         withReuse(true)
         withLabel("app-navn", "cpa-repo")
         start()
