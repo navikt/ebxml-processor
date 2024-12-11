16cd6d79f97778ddb474220d2b8bfcd02c918ae5
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2024-12-11 13:07:48 +0100

    Fikset payload tabell

--- ebms-provider/src/main/resources/db/migrations/V2__create_payload_table.sql
+++ ebms-provider/src/main/resources/db/migrations/V2__create_payload_table.sql
@@ -3,7 +3,8 @@ CREATE TABLE payload
     reference_id        VARCHAR(256)		NOT NULL,
     content_id		    VARCHAR(256)		NOT NULL,
     content_type		VARCHAR(256)		NOT NULL,
-    payload             TEXT		        NOT NULL,
+    content             BYTEA		        NOT NULL,
+    direction           VARCHAR(3)          NOT NULL CHECK (direction IN ('IN', 'OUT')),
     created_at          TIMESTAMP           DEFAULT now(),
-	PRIMARY KEY (reference_id)
+	PRIMARY KEY (reference_id, content_id)
 );
