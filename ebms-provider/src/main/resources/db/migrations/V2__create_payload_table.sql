545a3012f9884dee9f0260cb4a03b067fb6bf7fa
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2024-12-11 10:18:23 +0100

    Fikset SQL i migrasjoner

--- ebms-provider/src/main/resources/db/migrations/V2__create_payload_table.sql
+++ ebms-provider/src/main/resources/db/migrations/V2__create_payload_table.sql
@@ -4,6 +4,6 @@ CREATE TABLE payload
     content_id		    VARCHAR(256)		NOT NULL,
     content_type		VARCHAR(256)		NOT NULL,
     payload             TEXT		        NOT NULL,
-    created_at          TIMESTAMP           DEFAULT now()
+    created_at          TIMESTAMP           DEFAULT now(),
 	PRIMARY KEY (reference_id)
 );
