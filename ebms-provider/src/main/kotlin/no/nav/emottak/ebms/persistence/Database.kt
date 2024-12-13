f45c1578b2f30c398b3cf6773c8132b53a7e7559
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2024-12-13 11:44:20 +0100

    Stien til migrasjonsfiler blir satt når applokasjonen kjøres lokalt

--- ebms-provider/src/main/kotlin/no/nav/emottak/ebms/persistence/Database.kt
+++ ebms-provider/src/main/kotlin/no/nav/emottak/ebms/persistence/Database.kt
@@ -2,6 +2,7 @@ package no.nav.emottak.ebms.persistence
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import no.nav.emottak.util.getEnvVar
 import org.flywaydb.core.Flyway
 import org.jetbrains.exposed.sql.Database
 
@@ -17,9 +18,13 @@ class Database(
         Flyway.configure()
             .dataSource(migrationConfig.jdbcUrl, migrationConfig.username, migrationConfig.password)
             .initSql("SET ROLE \"$EBMS_DB_NAME-admin\"")
-            .locations("filesystem:src/main/resources/db/migrations")
             .lockRetryCount(50)
             .cleanDisabled(false) // TODO: Remove before merging.
+            .also {
+                if (getEnvVar("NAIS_CLUSTER_NAME", "local") == "local") {
+                    it.locations("filesystem:src/main/resources/db/migrations")
+                }
+            }
             .load()
             .also(Flyway::clean) // TODO: Remove before merging. So unfortunate, if you are seeing this in main.
             .migrate()
