a438dbf06b4f132c08f6dd76b1f234b34ae37f60
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2024-12-16 10:58:27 +0100

    Fjernet "direction" kolonnen

--- ebms-provider/src/main/resources/db/migrations/V2__create_payload_table.sql
+++ ebms-provider/src/main/resources/db/migrations/V2__create_payload_table.sql
@@ -4,7 +4,6 @@ CREATE TABLE payload
     content_id		    VARCHAR(256)		NOT NULL,
     content_type		VARCHAR(256)		NOT NULL,
     content             BYTEA		        NOT NULL,
-    direction           VARCHAR(3)          NOT NULL CHECK (direction IN ('IN', 'OUT')),
     created_at          TIMESTAMP           DEFAULT now(),
 	PRIMARY KEY (reference_id, content_id)
 );
