2fdaca39c305b7af84d1759977c3926a54bbb509
Thomas Burnett <thburnett@users.noreply.github.com>
2025-01-10 14:00:24 +0100

    Adding collect

--- ebms-provider/src/main/kotlin/no/nav/emottak/ebms/App.kt
+++ ebms-provider/src/main/kotlin/no/nav/emottak/ebms/App.kt
@@ -29,7 +29,8 @@ import io.micrometer.prometheus.PrometheusConfig
 import io.micrometer.prometheus.PrometheusMeterRegistry
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.awaitCancellation
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import net.logstash.logback.marker.Markers
 import no.nav.emottak.constants.SMTPHeaders
@@ -105,10 +106,10 @@ suspend fun startSignalReceiver(kafka: Kafka) {
     val signalProcessor = SignalProcessor()
     KafkaReceiver(receiverSettings)
         .receive(kafka.incomingSignalTopic)
-        .onEach { record ->
+        .map { record ->
             signalProcessor.processSignal(record.key(), record.value())
             record.offset.acknowledge()
-        }
+        }.collect()
 }
 
 fun Application.ebmsProviderModule(
