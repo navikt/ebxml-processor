db280415df3764083a41a08df7b7b9c8154c57f9
Thomas Burnett <thburnett@users.noreply.github.com>
2025-01-10 14:40:51 +0100

    Kafka pool for prod

--- ebms-provider/src/main/kotlin/no/nav/emottak/ebms/processing/SignalProcessor.kt
+++ ebms-provider/src/main/kotlin/no/nav/emottak/ebms/processing/SignalProcessor.kt
@@ -5,6 +5,7 @@ import no.nav.emottak.ebms.log
 class SignalProcessor {
 
     fun processSignal(reference: String, content: ByteArray) {
+        // TODO do something more useful with acknowledgments and errors
         log.info("Got signal with reference <$reference> and content: ${String(content)}")
     }
 }
