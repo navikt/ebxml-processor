ba8c207c2481310031b1313f4211794e0f64e48a
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2025-02-25 12:10:08 +0100

    Økt versjonen av emottak-utils bibliotek

--- emottak-utils/build.gradle.kts
+++ emottak-utils/build.gradle.kts
@@ -16,7 +16,7 @@ publishing {
         create<MavenPublication>("mavenJava") {
             groupId = "no.nav.emottak"
             artifactId = "emottak-utils"
-            version = "0.0.3"
+            version = "0.0.4"
             from(components["java"])
         }
     }
