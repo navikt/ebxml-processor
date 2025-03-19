53b649d56b1bd5dea9239ff2c4ebbec44862f2af
Kristian Frøhlich <186453+kfh@users.noreply.github.com>
2025-03-19 13:05:49 +0100

    Introduced usage of kotlin-kafka

--- emottak-utils/src/main/kotlin/no/nav/emottak/utils/events/EventLoggingService.kt
+++ emottak-utils/src/main/kotlin/no/nav/emottak/utils/events/EventLoggingService.kt
@@ -2,77 +2,16 @@ package no.nav.emottak.utils.events
 
 import no.nav.emottak.utils.events.model.Event
 import no.nav.emottak.utils.events.model.EventMessageDetails
-import no.nav.emottak.utils.events.model.EventType
 import no.nav.emottak.utils.kafka.KafkaPublisherClient
-import no.nav.emottak.utils.toJsonString
-import java.time.Instant
-import kotlin.uuid.ExperimentalUuidApi
-import kotlin.uuid.Uuid
 
-@OptIn(ExperimentalUuidApi::class)
-class EventLoggingService(private val kafkaPublisher: KafkaPublisherClient) {
+class EventLoggingService(
+    private val kafkaPublisherClient: KafkaPublisherClient
+) {
 
-    suspend fun logEvent(event: Event) {
-        kafkaPublisher.send(kafkaPublisher.topic, event.toByteArray())
+    private suspend fun logEvent(event: Event) {
+        kafkaPublisherClient.publishMessage(event.toByteArray())
     }
 
-    suspend fun logEventMessageDetails(eventMessageDetails: EventMessageDetails) {
-        kafkaPublisher.send(kafkaPublisher.topic, eventMessageDetails.toByteArray())
-    }
-
-    suspend fun logEventMessageDetails(
-        requestId: Uuid,
-        cpaId: String,
-        conversationId: String,
-        messageId: String,
-        refToMessageId: String? = null,
-        fromPartyId: String,
-        fromRole: String? = null,
-        toPartyId: String,
-        toRole: String? = null,
-        service: String,
-        action: String,
-        sentAt: Instant? = null
-    ) {
-        logEventMessageDetails(
-            EventMessageDetails(
-                requestId = requestId,
-                cpaId = cpaId,
-                conversationId = conversationId,
-                messageId = messageId,
-                refToMessageId = refToMessageId,
-                fromPartyId = fromPartyId,
-                fromRole = fromRole,
-                toPartyId = toPartyId,
-                toRole = toRole,
-                service = service,
-                action = action,
-                sentAt = sentAt
-            )
-        )
-    }
-
-    suspend fun logEventOK(eventType: EventType, requestId: Uuid, messageId: String, contentId: String? = null, eventData: String? = null) {
-        logEvent(
-            Event(
-                eventType = eventType,
-                requestId = requestId,
-                contentId = contentId,
-                messageId = messageId,
-                eventData = eventData
-            )
-        )
-    }
-
-    suspend fun logEventException(eventType: EventType, requestId: Uuid, messageId: String, ex: Exception, contentId: String? = null) {
-        logEvent(
-            Event(
-                eventType = eventType,
-                requestId = requestId,
-                contentId = contentId,
-                messageId = messageId,
-                eventData = ex.toJsonString()
-            )
-        )
-    }
+    private suspend fun logEventMessageDetails(eventMessageDetails: EventMessageDetails) =
+        kafkaPublisherClient.publishMessage(eventMessageDetails.toByteArray())
 }
