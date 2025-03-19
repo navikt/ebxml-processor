6008a43986153254459fea1023d8c433bee17ddc
Kristian Frøhlich <186453+kfh@users.noreply.github.com>
2025-03-19 13:13:37 +0100

    Added explicit return type

--- emottak-utils/src/main/kotlin/no/nav/emottak/utils/events/EventLoggingService.kt
+++ emottak-utils/src/main/kotlin/no/nav/emottak/utils/events/EventLoggingService.kt
@@ -3,15 +3,15 @@ package no.nav.emottak.utils.events
 import no.nav.emottak.utils.events.model.Event
 import no.nav.emottak.utils.events.model.EventMessageDetails
 import no.nav.emottak.utils.kafka.KafkaPublisherClient
+import org.apache.kafka.clients.producer.RecordMetadata
 
 class EventLoggingService(
     private val kafkaPublisherClient: KafkaPublisherClient
 ) {
 
-    private suspend fun logEvent(event: Event) {
+    private suspend fun logEvent(event: Event): Result<RecordMetadata> =
         kafkaPublisherClient.publishMessage(event.toByteArray())
-    }
 
-    private suspend fun logEventMessageDetails(eventMessageDetails: EventMessageDetails) =
+    private suspend fun logEventMessageDetails(eventMessageDetails: EventMessageDetails): Result<RecordMetadata> =
         kafkaPublisherClient.publishMessage(eventMessageDetails.toByteArray())
 }
