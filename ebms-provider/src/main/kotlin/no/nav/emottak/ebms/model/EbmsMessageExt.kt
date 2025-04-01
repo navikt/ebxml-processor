911be10bc7333cbf79551d9fc5bb9057656489e3
Thomas Burnett <thburnett@users.noreply.github.com>
2025-04-01 16:52:50 +0200

    Extra logging for debugging

--- ebms-provider/src/main/kotlin/no/nav/emottak/ebms/model/EbmsMessageExt.kt
+++ ebms-provider/src/main/kotlin/no/nav/emottak/ebms/model/EbmsMessageExt.kt
@@ -1,5 +1,6 @@
 package no.nav.emottak.ebms.model
 
+import no.nav.emottak.ebms.util.marker
 import no.nav.emottak.ebms.validation.SignaturValidator
 import no.nav.emottak.message.model.EbmsMessage
 import no.nav.emottak.message.model.PayloadMessage
@@ -7,6 +8,10 @@ import no.nav.emottak.message.model.SignatureDetails
 import no.nav.emottak.message.model.log
 
 fun EbmsMessage.sjekkSignature(signatureDetails: SignatureDetails) {
+    if (this is PayloadMessage) {
+        log.debug(this.marker(), "Debugging signature bug: ${this.payload}")
+        log.debug(this.marker(), "Debugging signature bug: ${String(this.payload.bytes)}")
+    }
     SignaturValidator.validate(signatureDetails, this.dokument!!, if (this is PayloadMessage) listOf(this.payload) else listOf())
-    log.info("Signatur OK")
+    log.info(this.marker(), "Signatur OK")
 }
