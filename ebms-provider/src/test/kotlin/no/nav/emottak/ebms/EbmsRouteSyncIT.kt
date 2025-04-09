bbaed9098074121861afff26c48e59fc1c41e2ac
Thomas Burnett <thburnett@users.noreply.github.com>
2025-04-09 15:47:03 +0200

    Added tests for missing start in Content-Type

--- ebms-provider/src/test/kotlin/no/nav/emottak/ebms/EbmsRouteSyncIT.kt
+++ ebms-provider/src/test/kotlin/no/nav/emottak/ebms/EbmsRouteSyncIT.kt
@@ -83,7 +83,7 @@ class EbmsRouteSyncIT : EbmsRoutFellesIT(SYNC_PATH) {
                     }
                     post("cpa/validate/{contentId}") {
                         val contentId = call.parameters["contentId"]
-                        if (contentId == "e491180e-eea6-41d6-ac5b-d232c9fb115f") {
+                        if (contentId == "e491180e-eea6-41d6-ac5b-d232c9fb115f" || (contentId?.startsWith("GENERERT") == true)) {
                             call.respond(
                                 ValidationResult(
                                     EbmsProcessing(),
@@ -137,7 +137,41 @@ class EbmsRouteSyncIT : EbmsRoutFellesIT(SYNC_PATH) {
         every {
             any<EbMSDocument>().signer(any())
         } returnsArgument(0)
-        val multipart = TestData.HarBorgerEgenandel.harBorgerEgenanderFritakRequestWithFormItem
+        val multipart = TestData.HarBorgerEgenandel.harBorgerEgenandelFritakRequestWithFormItem
+        val response = client.post(SYNC_PATH, multipart.asHttpRequest())
+        coVerify(exactly = 1) {
+            processingService.processSyncIn(any(), any())
+        }
+        assert(response.status == HttpStatusCode.OK)
+        println(String(response.readRawBytes()))
+    }
+
+    @Test
+    fun `Valid payload request without start in Content-Type should trigger processing and validation on way out`() = testSyncApp {
+        mockkStatic(EbMSDocument::signer)
+        every {
+            any<EbMSDocument>().signer(any())
+        } returnsArgument(0)
+        val multipart = TestData.HarBorgerEgenandel.harBorgerEgenandelFritakRequestWithFormItem
+        multipart.headers.modify {
+            it[MimeHeaders.CONTENT_TYPE] = TestData.HarBorgerEgenandel.MULTIPART_CONTENT_TYPE_WITHOUT_START
+        }
+        val response = client.post(SYNC_PATH, multipart.asHttpRequest())
+        coVerify(exactly = 1) {
+            processingService.processSyncIn(any(), any())
+        }
+        assert(response.status == HttpStatusCode.OK)
+        println(String(response.readRawBytes()))
+    }
+
+    @Test
+    fun `Valid payload request without start in Content-Type and Content-Id on Soap part should trigger processing and validation on way out`() = testSyncApp {
+        mockkStatic(EbMSDocument::signer)
+        every {
+            any<EbMSDocument>().signer(any())
+        } returnsArgument(0)
+        val multipart = TestData.HarBorgerEgenandel.harBorgerEgenandelFritakRequestWithoutBoundaryStartAndSoapPartContentId
+
         val response = client.post(SYNC_PATH, multipart.asHttpRequest())
         coVerify(exactly = 1) {
             processingService.processSyncIn(any(), any())
@@ -152,7 +186,7 @@ class EbmsRouteSyncIT : EbmsRoutFellesIT(SYNC_PATH) {
         every {
             any<EbMSDocument>().signer(any())
         } returnsArgument(0)
-        val multipart = TestData.HarBorgerEgenandel.harBorgerEgenanderFritakRequestWithFileItem
+        val multipart = TestData.HarBorgerEgenandel.harBorgerEgenandelFritakRequestWithFileItem
         val response = client.post(SYNC_PATH, multipart.asHttpRequest())
         coVerify(exactly = 1) {
             processingService.processSyncIn(any(), any())
@@ -184,7 +218,7 @@ class EbmsRouteSyncIT : EbmsRoutFellesIT(SYNC_PATH) {
                 }
             }
         }
-        val multipart = TestData.HarBorgerEgenandel.harBorgerEgenanderFritakRequestWithFormItem
+        val multipart = TestData.HarBorgerEgenandel.harBorgerEgenandelFritakRequestWithFormItem
         val response = client.post(SYNC_PATH, multipart.asHttpRequest())
         coVerify(exactly = 1) {
             processingService.processSyncIn(any(), any())
@@ -202,6 +236,8 @@ internal class TestData {
         companion object {
             val MULTIPART_CONTENT_TYPE =
                 """multipart/related; boundary="------=_part_c2a3282c_e754_4193_9fb5_e8a469941f97"; type="text/xml"; start="soapId-c1a5d7ed-3850-4a74-ba28-8d73f5355336"""
+            val MULTIPART_CONTENT_TYPE_WITHOUT_START =
+                """multipart/related; boundary="------=_part_c2a3282c_e754_4193_9fb5_e8a469941f97"; type="text/xml"""
             val EBXML_PAYLOAD =
                 """<?xml version="1.0" encoding="UTF-8"?><SOAP:Envelope xsi:schemaLocation="http://schemas.xmlsoap.org/soap/envelope/ http://www.oasis-open.org/committees/ebxml-msg/schema/envelope.xsd http://www.oasis-open.org/committees/ebxml-msg/schema/msg-header-2_0.xsd" xmlns:eb="http://www.oasis-open.org/committees/ebxml-msg/schema/msg-header-2_0.xsd" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP="http://schemas.xmlsoap.org/soap/envelope/"><SOAP:Header><eb:MessageHeader eb:version="2.0" SOAP:mustUnderstand="1"><eb:From><eb:PartyId eb:type="HER">8090595</eb:PartyId><eb:Role>Utleverer</eb:Role></eb:From><eb:To><eb:PartyId eb:type="HER">79768</eb:PartyId><eb:Role>Frikortregister</eb:Role></eb:To><eb:CPAId>nav:qass:31162</eb:CPAId><eb:ConversationId>e17eb03e-9e43-43fb-874c-1fde9a28c308</eb:ConversationId><eb:Service eb:type="string">HarBorgerEgenandelFritak</eb:Service><eb:Action>EgenandelForesporsel</eb:Action><eb:MessageData><eb:MessageId>e17eb03e-9e43-43fb-874c-1fde9a28c308</eb:MessageId><eb:Timestamp>2024-02-12T13:04:02.541Z</eb:Timestamp></eb:MessageData></eb:MessageHeader><eb:SyncReply eb:version="2.0" SOAP:mustUnderstand="1" SOAP:actor="http://schemas.xmlsoap.org/soap/actor/next"/><Signature xmlns="http://www.w3.org/2000/09/xmldsig#"><SignedInfo><CanonicalizationMethod Algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315"/><SignatureMethod Algorithm="http://www.w3.org/2000/09/xmldsig#rsa-sha1"/><Reference URI=""><Transforms><Transform Algorithm="http://www.w3.org/2000/09/xmldsig#enveloped-signature"/><Transform Algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315"/></Transforms><DigestMethod Algorithm="http://www.w3.org/2000/09/xmldsig#sha1"/><DigestValue>8soClBWhwJWIJi4uo0BvfN/tNlY=</DigestValue></Reference><Reference URI="cid:attachmentId-3b407d6f-7efc-4ce9-99a6-868f04329e68"><DigestMethod Algorithm="http://www.w3.org/2000/09/xmldsig#sha1"/><DigestValue>ch2SFbU7KFmNm6Jp5zWa1dGVWCQ=</DigestValue></Reference></SignedInfo><SignatureValue>Q0Op2SZFDiQqp+zt0GX9mZ8Q8LGqIKmAsfc+l9X7mlgmKtOyQ0MBDo4ebzoRX4GLviWBkF2TMTRvWluw87blUJSga2I226yVC4GaTjhNhP50Rkvxd9olhHWSs/E/JrpT/X7KFmVDAR5A+mRxPKFMaKDkS32IHlcyi6x8jA2HSUZxD1pkfALj2B8EuL/39e5BecfOkHOyHzj/Nz0gvIgldYKhaTf81GAgDYuQ/STggXtJ7FbsVeXjIN9mdJyrq4DdO11StMS+ET5WQrtn5egRkzzuDLtwYL4IB4oFSZ8bYr2hNcFhWYYq5IvKWCU93AlKktbIYaZ10AtigYUYQTkBFWlqgHPhsN+5IDcqgXguJTSEh9pbgKf4W1RzuzgjxNvPCAV5D7LAyVp1QLG+n6hUgFYRO0QY1LkCZ+Vs7YJrgbBZyjwjBq1gZz6Y9B4jHBPBpjKFxN/0p4CGJ5G8YTumMMPwuxyP8XiYnKShF/IzM7VAOfwYmhnm/mw2mEItmly3</SignatureValue><KeyInfo><X509Data><X509Certificate>MIIGaDCCBFCgAwIBAgILAZtVR0IY/1EvjtowDQYJKoZIhvcNAQELBQAwbjELMAkGA1UEBhMCTk8xGDAWBgNVBGEMD05UUk5PLTk4MzE2MzMyNzETMBEGA1UECgwKQnV5cGFzcyBBUzEwMC4GA1UEAwwnQnV5cGFzcyBDbGFzcyAzIFRlc3Q0IENBIEcyIFNUIEJ1c2luZXNzMB4XDTIzMDgyODA4MzQzM1oXDTI2MDgyODIxNTkwMFowgYsxCzAJBgNVBAYTAk5PMRwwGgYDVQQKDBNBUE9URUsgMSBHUlVQUEVOIEFTMSkwJwYDVQQLDCBFUjpOTy05ODQ4ODYxMzYtVEVTVCBBMSBIYXVnZXJ1ZDEZMBcGA1UEAwwQVEVTVCBBMSBIYXVnZXJ1ZDEYMBYGA1UEYQwPTlRSTk8tOTgzMDQ0Nzc4MIIBojANBgkqhkiG9w0BAQEFAAOCAY8AMIIBigKCAYEA1HE+rxisqKWgFOnqAtyig+2vpadevrgTfbhQsZ4A0tevblgURDat2y5cMMqOI5FAXym+0KGooCdupZZGb1G9mmII5/clW2FkeeqvVBcJ5WCUynEuYd+v6dhMAdvgc3ZNgdlqey3amXyhF9YWkQPoaBLyfIjYzsBak3A5FfAUmcCAt3Fh2k7SkfvGypYVVEDeKBFnD/sxAuWBgolc+go+ylvSZdHbAEsaHGJAXBz3fTFKkPwng8dRPQRYCd34/FcKXdl15U2CdOLkMiqy4+qQRckJFKppa40COR7WVStJ3Tq41VqxDrPeqoigUySmNQ2g27nXS7EpPfKoKr/MSVo92euna4yLtKXnbUi9vPcmnb6BQn0CNe4YesHCq7Evt7Cc0ksxBzxN4YXNkCD+EonMnTL0d5K937Pm6QCF0UMHE55UmrCXKYAPsKkhmicfVz7+nhg5+ZDKmjY6rjNC2ro23Jy6B9jdu7v4/J+55Tsq+XWrcgX6RD6M4WV4HDH7HkTrAgMBAAGjggFnMIIBYzAJBgNVHRMEAjAAMB8GA1UdIwQYMBaAFKf+u2xZiK10LkZeemj50bu/z7aLMB0GA1UdDgQWBBQe9sgFd28eFxkwaNNs9uOuEIfIpTAOBgNVHQ8BAf8EBAMCBkAwHwYDVR0gBBgwFjAKBghghEIBGgEDAjAIBgYEAI96AQEwQQYDVR0fBDowODA2oDSgMoYwaHR0cDovL2NybC50ZXN0NC5idXlwYXNzY2EuY29tL0JQQ2wzQ2FHMlNUQlMuY3JsMHsGCCsGAQUFBwEBBG8wbTAtBggrBgEFBQcwAYYhaHR0cDovL29jc3Bicy50ZXN0NC5idXlwYXNzY2EuY29tMDwGCCsGAQUFBzAChjBodHRwOi8vY3J0LnRlc3Q0LmJ1eXBhc3NjYS5jb20vQlBDbDNDYUcyU1RCUy5jZXIwJQYIKwYBBQUHAQMEGTAXMBUGCCsGAQUFBwsCMAkGBwQAi+xJAQIwDQYJKoZIhvcNAQELBQADggIBAGNRk/LOxpWr4gGgF6iJYdu3m5eWqYTlfX2XI9irs1Xg4kY+TPNyhAu5p7wEZ3olT3f0xnnhzSLN6dl942dJwVdVNmrPQj9hoduxC2+79PKMOwFInbaKV+dRrJe1C/cMmCtCOnUfRQP7nG91Yy2t1XS8KDGZPo+ZxpT8Knhywi/cGjQJrx0JR+LR6EYeFPdafEBoBhnHGpbqWRbj/Ee+POMieK8S7UE62grcw1IvowUwm2PSARrRDl3BFduxOZq9+AImpQvP+i6hXu591jRAwn7bJC7WvY/xEgfDB7tgsHdojT5jbjhlAtUCpaENTM6J2bmhBxAORb+4rnV2cmuuOzr0m9LiJoanNEZFpGp4BFe40SEvpohpuZ1qFJlX1TyRcnl+CtUHLmtIsMpsgMJh0gGA6vVnHjyO9BA669z6NalHad/mdKdRzVM80qP2NsGiK8esM9q7WV20VmD2XTwmQBVxohsXg2TGr7V0+8mwudJ2kB0LzPiAaiSZmb2QP0qctY+HkJvZFMNMg26Bg+q3LWbJaGJWHwmehXYu3ttpWkd+5RhGZFmCx944PdQ2e0+EGUTKjbDwLKPizFGnVlnSDjCVSynUGB3GFkPqlv02FQthsidGXdZjJz9fLpuG4y0aQZiMZKtehJHQ2YQeAn/6xkZpBSIbokL9NjuWya8Qp0gl</X509Certificate></X509Data><KeyValue><RSAKeyValue><Modulus>1HE+rxisqKWgFOnqAtyig+2vpadevrgTfbhQsZ4A0tevblgURDat2y5cMMqOI5FAXym+0KGooCdupZZGb1G9mmII5/clW2FkeeqvVBcJ5WCUynEuYd+v6dhMAdvgc3ZNgdlqey3amXyhF9YWkQPoaBLyfIjYzsBak3A5FfAUmcCAt3Fh2k7SkfvGypYVVEDeKBFnD/sxAuWBgolc+go+ylvSZdHbAEsaHGJAXBz3fTFKkPwng8dRPQRYCd34/FcKXdl15U2CdOLkMiqy4+qQRckJFKppa40COR7WVStJ3Tq41VqxDrPeqoigUySmNQ2g27nXS7EpPfKoKr/MSVo92euna4yLtKXnbUi9vPcmnb6BQn0CNe4YesHCq7Evt7Cc0ksxBzxN4YXNkCD+EonMnTL0d5K937Pm6QCF0UMHE55UmrCXKYAPsKkhmicfVz7+nhg5+ZDKmjY6rjNC2ro23Jy6B9jdu7v4/J+55Tsq+XWrcgX6RD6M4WV4HDH7HkTr</Modulus><Exponent>AQAB</Exponent></RSAKeyValue></KeyValue></KeyInfo></Signature></SOAP:Header><SOAP:Body><eb:Manifest eb:version="2.0"><eb:Reference xlink:href="cid:attachmentId-3b407d6f-7efc-4ce9-99a6-868f04329e68"/></eb:Manifest></SOAP:Body></SOAP:Envelope>"""
             val FAGMELDING_PAYLOAD = """PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48bnM6TXNnSGVhZCB4bWxuczp4
@@ -257,6 +293,9 @@ ZWZEb2M+CiAgPC9uczpEb2N1bWVudD4KPC9uczpNc2dIZWFkPg=="""
                 append(MimeHeaders.CONTENT_TRANSFER_ENCODING, "binary")
                 append(MimeHeaders.CONTENT_TYPE, """text/xml; charset="UTF-8""")
             }
+            val validSoapMimeHeadersWithoutContentId = validSoapMimeHeaders.modify {
+                it.remove(MimeHeaders.CONTENT_ID)
+            }
 
             val validSoapAttachmentHeadersFormItem = Headers.build {
                 append(MimeHeaders.CONTENT_ID, "<attachmentId-3b407d6f-7efc-4ce9-99a6-868f04329e68>")
@@ -265,20 +304,28 @@ ZWZEb2M+CiAgPC9uczpEb2N1bWVudD4KPC9uczpNc2dIZWFkPg=="""
                 append(MimeHeaders.CONTENT_DISPOSITION, "attachment")
             }
 
-            val validSoapAttachmentHeadersFileItem = Headers.build {
-                append(MimeHeaders.CONTENT_ID, "<attachmentId-3b407d6f-7efc-4ce9-99a6-868f04329e68>")
-                append(MimeHeaders.CONTENT_TRANSFER_ENCODING, "base64")
-                append(MimeHeaders.CONTENT_TYPE, """application/pkcs7-mime""")
-                append(MimeHeaders.CONTENT_DISPOSITION, "attachment; filename=attachmentId-3b407d6f-7efc-4ce9-99a6-868f04329e68")
+            val validSoapAttachmentHeadersFileItem = validSoapAttachmentHeadersFormItem.modify {
+                it[MimeHeaders.CONTENT_DISPOSITION] = "attachment; filename=attachmentId-3b407d6f-7efc-4ce9-99a6-868f04329e68"
             }
-            val harBorgerEgenanderFritakRequestWithFormItem = MultipartRequest(
+
+            val harBorgerEgenandelFritakRequestWithoutBoundaryStartAndSoapPartContentId = MultipartRequest(
+                valid.modify {
+                    it[MimeHeaders.CONTENT_TYPE] = MULTIPART_CONTENT_TYPE_WITHOUT_START
+                },
+                listOf(
+                    Part(validSoapMimeHeadersWithoutContentId, EBXML_PAYLOAD),
+                    Part(validSoapAttachmentHeadersFormItem, FAGMELDING_PAYLOAD)
+                )
+            )
+
+            val harBorgerEgenandelFritakRequestWithFormItem = MultipartRequest(
                 valid,
                 listOf(
                     Part(validSoapMimeHeaders, EBXML_PAYLOAD),
                     Part(validSoapAttachmentHeadersFormItem, FAGMELDING_PAYLOAD)
                 )
             )
-            val harBorgerEgenanderFritakRequestWithFileItem = MultipartRequest(
+            val harBorgerEgenandelFritakRequestWithFileItem = MultipartRequest(
                 valid,
                 listOf(
                     Part(validSoapMimeHeaders, EBXML_PAYLOAD),
