82d78560335234f4f00d0d4820d4dd087f184871
terjenilssen <terje.nilssen@nav.no>
2025-05-12 14:01:15 +0200

    Korrigert kommentar.

--- ebxml-processing-model/src/main/kotlin/no/nav/emottak/message/util/MimeUtil.kt
+++ ebxml-processing-model/src/main/kotlin/no/nav/emottak/message/util/MimeUtil.kt
@@ -12,6 +12,6 @@ private val hostName = when (getEnvVar("NAIS_CLUSTER_NAME", "local")) {
     else -> "local"
 }
 
-// For å slippe avhengighet til emottak-utils (kopiert fra EnvUtil.kt):
+// For å slippe avhengighet til emottak-utils (kopiert fra EnvUtils.kt):
 private fun getEnvVar(varName: String, defaultValue: String? = null) =
     System.getProperty(varName) ?: System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Environment: Missing required variable \"$varName\"")
