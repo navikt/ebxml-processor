2bcce9aecc1765ac52f16b3a384113705f89646d
terjenilssen <terje.nilssen@nav.no>
2025-07-29 11:53:19 +0200

    ktlintFormat

--- ebms-payload/src/test/kotlin/no/nav/emottak/payload/AppRecUtilTest.kt
+++ ebms-payload/src/test/kotlin/no/nav/emottak/payload/AppRecUtilTest.kt
@@ -1,8 +1,8 @@
 package no.nav.emottak.payload
 
 import no.kith.xmlstds.msghead._2006_05_24.MsgHead
-import no.nav.emottak.payload.apprec.melding.AppRecErrorCode
 import no.nav.emottak.payload.apprec.createNegativeApprec
+import no.nav.emottak.payload.apprec.melding.AppRecErrorCode
 import no.nav.emottak.payload.util.unmarshal
 import no.nav.emottak.util.signatur.SignatureException
 import org.junit.jupiter.api.Assertions.assertEquals
