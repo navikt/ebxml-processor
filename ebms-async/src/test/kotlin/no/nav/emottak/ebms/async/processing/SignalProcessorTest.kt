c75cf136e987039347a2751dc7c47f4afc928da1
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2025-08-21 11:05:01 +0200

    Gjenopprettet SignalProcessorTest

---
+++ ebms-async/src/test/kotlin/no/nav/emottak/ebms/async/processing/SignalProcessorTest.kt
@@ -0,0 +1,44 @@
+package no.nav.emottak.ebms.async.processing
+
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import no.nav.emottak.ebms.validation.CPAValidationService
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.util.UUID
+import javax.xml.bind.UnmarshalException
+
+class SignalProcessorTest {
+
+    val cpaValidationService = mockk<CPAValidationService>()
+    val signalMessageService = SignalMessageService(cpaValidationService)
+
+    @Test
+    @Disabled
+    fun `Payload message throws error`() {
+        val message = this::class.java.classLoader
+            .getResourceAsStream("signaltest/payloadmessage.xml")
+
+        val exception = assertThrows<RuntimeException> {
+            runBlocking {
+                signalMessageService.processSignal(UUID.randomUUID().toString(), message!!.readAllBytes())
+            }
+        }
+        assertEquals("Unrecognized dokument type", exception.message)
+    }
+
+    @Test
+    @Disabled
+    fun `Not ebxml message throws error`() {
+        val message = this::class.java.classLoader
+            .getResourceAsStream("signaltest/dokument.xml")
+
+        assertThrows<UnmarshalException> {
+            runBlocking {
+                signalMessageService.processSignal(UUID.randomUUID().toString(), message!!.readAllBytes())
+            }
+        }
+    }
+}
