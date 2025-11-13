888d4a73b0e6061068fb96621359d03b6f1669d8
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2025-11-13 13:16:40 +0100

    Lagt til SerialName innstillinger for ErrorMessage

--- ebms-provider/src/main/kotlin/no/nav/emottak/ebms/ErrorMessage.kt
+++ ebms-provider/src/main/kotlin/no/nav/emottak/ebms/ErrorMessage.kt
@@ -1,12 +1,22 @@
 package no.nav.emottak.edi
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ErrorMessage(
+    @SerialName("Error")
     val error: String? = null,
+
+    @SerialName("ErrorCode")
     val errorCode: Int,
+
+    @SerialName("ValidationErrors")
     val validationErrors: List<String>? = null,
+
+    @SerialName("StackTrace")
     val stackTrace: String? = null,
+
+    @SerialName("RequestId")
     val requestId: String
 )
