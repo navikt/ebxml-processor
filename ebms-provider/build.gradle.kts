aac0c792d437af57bf02ea0dc51721d44bd11198
Thomas Burnett <thburnett@users.noreply.github.com>
2025-12-09 15:16:45 +0100

    Update payload xsd dependency due to vulnerabilities

--- ebms-provider/build.gradle.kts
+++ ebms-provider/build.gradle.kts
@@ -69,7 +69,6 @@ dependencies {
     implementation("com.sun.xml.messaging.saaj:saaj-impl:3.0.2")
     implementation(libs.emottak.payload.xsd)
     implementation(libs.emottak.utils)
-    // implementation("org.glassfish.jaxb:jaxb-runtime:4.0.3") // TODO: Latest. Krever at protokoll oppdateres
     implementation(libs.ebxml.protokoll)
     implementation(libs.token.validation.ktor.v3)
 
