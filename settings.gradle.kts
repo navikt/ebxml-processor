dd7ea9f329f06a9a2911407240a6b014d336cc7b
terjenilssen <terje.nilssen@nav.no>
2025-12-15 15:42:30 +0100

    Oppdatert til nyeste versjon av emottak-payload-xsd.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -70,7 +70,7 @@ dependencyResolutionManagement {
             library("jackson-dataformat-yaml", "com.fasterxml.jackson.dataformat", "jackson-dataformat-yaml").versionRef("fasterxml-jackson")
 
             library("ebxml-protokoll", "no.nav.emottak:ebxml-protokoll:0.0.7")
-            library("emottak-payload-xsd", "no.nav.emottak:emottak-payload-xsd:0.0.10")
+            library("emottak-payload-xsd", "no.nav.emottak:emottak-payload-xsd:0.0.11")
             library("emottak-utils", "no.nav.emottak", "emottak-utils").versionRef("emottak-utils")
             library("hikari", "com.zaxxer:HikariCP:5.0.1")
             library("labai-jsr305x-annotations", "com.github.labai:labai-jsr305x-annotations:0.0.2")
