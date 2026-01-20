3acbdfef7b35fce489e467bb8186877a3222bd88
Aage Kramarics <Aage.Kramarics@nav.no>
2026-01-20 14:55:57 +0100

    KtLint/format

--- ebms-async/src/test/kotlin/no/nav/emottak/ebms/async/persistence/ResponseAckRepositoryTest.kt
+++ ebms-async/src/test/kotlin/no/nav/emottak/ebms/async/persistence/ResponseAckRepositoryTest.kt
@@ -1,27 +1,22 @@
 package no.nav.emottak.ebms.async.persistence
 
-import kotlinx.coroutines.runBlocking
 import no.nav.emottak.ebms.async.ebmsPostgres
 import no.nav.emottak.ebms.async.persistence.repository.ResponseAckRepository
 import no.nav.emottak.ebms.async.testConfiguration
 import no.nav.emottak.message.ebxml.messageHeader
-import no.nav.emottak.message.model.AsyncPayload
 import no.nav.emottak.message.model.EmailAddress
 import no.nav.emottak.message.xml.XmlMarshaller
-import no.nav.emottak.message.xml.createDocument
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.oasis_open.committees.ebxml_cppa.schema.cpp_cpa_2_0.EndpointTypeType
-import org.oasis_open.committees.ebxml_msg.schema.msg_header_2_0.MessageData
 import org.oasis_open.committees.ebxml_msg.schema.msg_header_2_0.MessageHeader
 import org.testcontainers.containers.PostgreSQLContainer
 import org.xmlsoap.schemas.soap.envelope.Envelope
 import java.sql.DriverManager
 import java.time.Instant
-import kotlin.uuid.Uuid
 
 class ResponseAckRepositoryTest {
     companion object {
@@ -84,7 +79,7 @@ class ResponseAckRepositoryTest {
         Assertions.assertEquals(false, responses[0].ackReceived)
         Assertions.assertEquals(xmlMarshaller.marshal(messageHeader), xmlMarshaller.marshal(responses[0].messageHeader))
         Assertions.assertEquals(payload, String(responses[0].messageContent))
-        Assertions.assertEquals(listOf(email1,email2), responses[0].emailAddressList)
+        Assertions.assertEquals(listOf(email1, email2), responses[0].emailAddressList)
         Assertions.assertTrue { responses[0].firstSent.isBefore(Instant.now()) }
         Assertions.assertTrue { responses[0].lastSent.isBefore(Instant.now()) }
         Assertions.assertEquals(0, responses[0].resentCount)
@@ -158,8 +153,10 @@ class ResponseAckRepositoryTest {
     }
 
     private fun readMessageHeaderFromTestFile(fileName: String): MessageHeader {
-        val testMessage = String(this::class.java.classLoader
-                .getResourceAsStream(fileName)!!.readAllBytes())
+        val testMessage = String(
+            this::class.java.classLoader
+                .getResourceAsStream(fileName)!!.readAllBytes()
+        )
         return xmlMarshaller.unmarshal(testMessage, Envelope::class.java).header.messageHeader()
     }
 }
