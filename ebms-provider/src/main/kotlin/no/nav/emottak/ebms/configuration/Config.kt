abe8520d2ed365112f571bd5641f25bff1a711ee
Thomas Burnett <thburnett@users.noreply.github.com>
2026-01-29 12:11:46 +0100

    Flytter async kafka config fra felles til async

--- ebms-provider/src/main/kotlin/no/nav/emottak/ebms/configuration/Config.kt
+++ ebms-provider/src/main/kotlin/no/nav/emottak/ebms/configuration/Config.kt
@@ -1,9 +1,5 @@
 package no.nav.emottak.ebms.configuration
 
-import no.nav.emottak.config.KafkaPayloadProducer
-import no.nav.emottak.config.KafkaPayloadReceiver
-import no.nav.emottak.config.KafkaSignalProducer
-import no.nav.emottak.config.KafkaSignalReceiver
 import no.nav.emottak.util.KeyStoreConfiguration
 import no.nav.emottak.utils.config.EventLogging
 import no.nav.emottak.utils.config.Kafka
@@ -11,9 +7,5 @@ import no.nav.emottak.utils.config.Kafka
 data class Config(
     val kafka: Kafka,
     val eventLogging: EventLogging,
-    val kafkaSignalReceiver: KafkaSignalReceiver,
-    val kafkaSignalProducer: KafkaSignalProducer,
-    val kafkaPayloadReceiver: KafkaPayloadReceiver,
-    val kafkaPayloadProducer: KafkaPayloadProducer,
     val signering: List<KeyStoreConfiguration>
 )
