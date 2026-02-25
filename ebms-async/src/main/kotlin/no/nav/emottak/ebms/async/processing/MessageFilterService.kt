5f23c9dd886f72b129f0c84487a269e062ce00a0
roman.c-e <rce@zivue.com>
2026-02-25 10:21:57 +0100

    json handling for filtermessage/rerun

--- ebms-async/src/main/kotlin/no/nav/emottak/ebms/async/processing/MessageFilterService.kt
+++ ebms-async/src/main/kotlin/no/nav/emottak/ebms/async/processing/MessageFilterService.kt
@@ -1,7 +1,7 @@
 package no.nav.emottak.ebms.async.processing
 
 import io.github.nomisRev.kafka.receiver.ReceiverRecord
-import kotlinx.serialization.encodeToString
+import io.ktor.http.ContentType
 import kotlinx.serialization.json.Json
 import no.nav.emottak.ebms.SmtpTransportClient
 import no.nav.emottak.ebms.async.util.EventRegistrationService
@@ -14,6 +14,7 @@ import no.nav.emottak.message.model.Payload
 import no.nav.emottak.message.model.PayloadMessage
 import no.nav.emottak.message.model.documentType
 import no.nav.emottak.message.xml.createDocument
+import no.nav.emottak.utils.common.model.SendInResponse
 import no.nav.emottak.utils.common.parseOrGenerateUuid
 import no.nav.emottak.utils.kafka.model.EventDataType
 import no.nav.emottak.utils.kafka.model.EventType
@@ -28,6 +29,28 @@ open class MessageFilterService(
 ) {
 
     open suspend fun filterMessage(record: ReceiverRecord<String, ByteArray>) {
+        val sendInResponse = runCatching {
+            Json.decodeFromString<SendInResponse>(record.value().decodeToString())
+        }.getOrNull()
+
+        if (sendInResponse != null) {
+            val cpaId = record.headers().lastHeader("cpaId")?.let { String(it.value()) } ?: ""
+            val refToMessageId = record.headers().lastHeader("refToMessageId")?.let { String(it.value()) }
+            val payloadMessage = PayloadMessage(
+                requestId = sendInResponse.requestId,
+                messageId = sendInResponse.messageId,
+                conversationId = sendInResponse.conversationId,
+                cpaId = cpaId,
+                addressing = sendInResponse.addressing,
+                payload = Payload(sendInResponse.payload, ContentType.Application.Xml.toString()),
+                refToMessageId = refToMessageId,
+                duplicateElimination = false,
+                ackRequested = true
+            )
+            payloadMessageService.processOutboundResponse(record, payloadMessage)
+            return
+        }
+
         val ebmsMessage = createEbmsDocument(
             requestId = record.key(),
             document = record.value().createDocument()
