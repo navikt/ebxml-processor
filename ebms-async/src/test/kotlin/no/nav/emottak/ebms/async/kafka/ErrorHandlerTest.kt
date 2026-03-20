c4a29155d43e167b35b7d9ab32c7ff4dd9f6d605
Chris (RettIProd) Olsen <christopher.olsen@nav.no>
2026-03-20 15:25:28 +0100

    Trodde den var tostring?

--- ebms-async/src/test/kotlin/no/nav/emottak/ebms/async/kafka/ErrorHandlerTest.kt
+++ ebms-async/src/test/kotlin/no/nav/emottak/ebms/async/kafka/ErrorHandlerTest.kt
@@ -47,14 +47,14 @@ class ErrorHandlerTest {
             val processedMessages = ArrayList<ReceiverRecord<String, ByteArray>>()
             val messageFilterService = DummyMessageFilterService(errorHandler, processedMessages)
 
-            errorHandler.sendToRetry(newRecord("test-message"), direction = Direction.IN, nextRetryTime = Instant.now())
+            errorHandler.sendToRetry(newRecord("test-message"), direction = Direction.IN, nextRetryTime = Instant.now().toString())
             val record1 = getRecordFromErrorQueueAtOffset(testcontainerKafkaConfig, 0)
             assertTrue(record1?.key() == "test-message", "Melding sendt til feilhåndtering ligger på feilkø med offset 0")
 
             retryService.consumeRetryQueueIncoming(1, messageFilterService::filterMessage)
             assertTrue(processedMessages.size == 1, "Etter prosessering av feilkø er meldingen prosessert av MessageFilterService")
 
-            errorHandler.sendToRetry(newRecord("failingAtFirstRetry"), direction = Direction.IN, nextRetryTime = Instant.now())
+            errorHandler.sendToRetry(newRecord("failingAtFirstRetry"), direction = Direction.IN, nextRetryTime = Instant.now().toString())
             val record2 = getRecordFromErrorQueueAtOffset(testcontainerKafkaConfig, 1)
             assertTrue(record2?.key() == "failingAtFirstRetry", "Melding som vil feile ligger på feilkø med offset 1")
 
