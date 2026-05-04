e71ad6912445ea5fb4c62910b4e1ea5da13585b0
Chris (RettIProd) Olsen <christopher.olsen@nav.no>
2026-05-04 15:11:29 +0200

    Default dersom ingen funnet

--- cpa-repo/src/main/kotlin/no/nav/emottak/cpa/CPAUtil.kt
+++ cpa-repo/src/main/kotlin/no/nav/emottak/cpa/CPAUtil.kt
@@ -65,10 +65,10 @@ fun List<org.oasis_open.committees.ebxml_cppa.schema.cpp_cpa_2_0.PartyId>.actual
 fun PartyInfo.toDomainModel(service: String, action: String): Party {
     return Party(
         partyId.map { partyId -> PartyId(partyId.type!!, partyId.value!!) },
-        collaborationRole.first() { collabRole ->
+        collaborationRole.firstOrNull() { collabRole ->
             (collabRole.serviceBinding.service.value == service || EBMS_SERVICE_URI == service) &&
                 collabRole.serviceBinding.canSend.any { cs -> cs.thisPartyActionBinding.action == action }
-        }.role.name
+        }?.role?.name ?: "Default".also { log.warn("Fant ikke gyldig rolle for service $service og action $action, setter default") }
     )
 }
 
