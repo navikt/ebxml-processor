51d1c6573165b810ed604a3fe9d087f60f843bac
Thomas Burnett <7643677+thburnett@users.noreply.github.com>
2026-08-26 14:37:56 +0200

    Setter korrekt messageId for signal events (#318)
    Co-authored-by: Thomas Burnett <thburnett@users.noreply.github.com>

--- ebms-async/src/main/kotlin/no/nav/emottak/ebms/async/processing/SignalMessageService.kt
+++ ebms-async/src/main/kotlin/no/nav/emottak/ebms/async/processing/SignalMessageService.kt
@@ -49,13 +49,12 @@ class SignalMessageService(
             message = acknowledgment,
             checkSignature = checkSignature
         )
-        if (acknowledgment.addressing.from.role == "Not applicable") {
-            eventRegistrationService.registerEvent(
-                eventType = EventType.MESSAGEFLOW_COMPLETED,
-                conversationId = acknowledgment.conversationId,
-                requestId = acknowledgment.requestId.parseOrGenerateUuid()
-            )
-        }
+        eventRegistrationService.registerEvent(
+            eventType = EventType.MESSAGEFLOW_COMPLETED,
+            conversationId = acknowledgment.conversationId,
+            messageId = acknowledgment.refToMessageId,
+            requestId = acknowledgment.requestId.parseOrGenerateUuid()
+        )
         log.info(acknowledgment.marker(), "Got acknowledgment with requestId <${acknowledgment.requestId}>")
         messagePendingAckRepository.registerAckForMessage(acknowledgment.refToMessageId)
     }
@@ -73,7 +72,7 @@ class SignalMessageService(
             eventRegistrationService.registerEvent(
                 eventType = EventType.UNKNOWN_ERROR_OCCURRED,
                 requestId = messageError.requestId.parseOrGenerateUuid(),
-                messageId = messageError.messageId,
+                messageId = messageError.refToMessageId,
                 eventData = Json.encodeToString(
                     mapOf(
                         EventDataType.ERROR_MESSAGE to "${error.code}: ${error.descriptionText}"
